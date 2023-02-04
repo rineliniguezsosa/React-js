cce818957a6a138a69888004baa422cc8a9c638e
rinel <rineliniguezsosa@gmail.com>
2023-02-03 23:47:47 -0600

    comentarios en la conversion de typescript

--- intro-javascript/src/index.js
+++ intro-javascript/src/index.js
@@ -1,4 +1,15 @@
 //DESESTRUCTURANDO OBJETOS
+// interface Heroe {
+//     nombre:string,
+//     edad:number,
+//     clave:string,
+// }
+
+// const heroe:Heroe = {
+//     nombre:'tony stark',
+//     edad:35,
+//     clave:'iron man'
+// }
 const heroe = {
     nombre:'tony stark',
     edad:35,
@@ -8,6 +19,9 @@ const heroe = {
 //const {nombre,edad,clave} = heroe;
 //console.log(nombre)
 
+// const retornapersona = ({nombre}):void =>{
+//     console.log(nombre)
+// }
 const retornapersona = ({nombre}) =>{
     console.log(nombre)
 }
