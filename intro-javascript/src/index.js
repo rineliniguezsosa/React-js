b95ac6fce950efc508c78ece3eed1b485a4e5cae
rinel <rineliniguezsosa@gmail.com>
2023-02-06 11:33:49 -0600

    estableciendo try catch para verificar que la consulta  a la api se ejecute correctamente o no

--- intro-javascript/src/index.js
+++ intro-javascript/src/index.js
@@ -1,9 +1,13 @@
 
 const getImage = async() =>{
-    const apikey = 'F8OVeoJwy53KS0TI8S4VBdpmemu9QrXx';
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}`;
-    const resp = await fetch(url);
-    console.log(resp)
+    try{
+        const apikey = 'F8OVeoJwy53KS0TI8S4VBdpmemu9QrXx';
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}`;
+        const resp = await fetch(url);
+        console.log(resp)
+    }catch(e){
+        console.log('Algo salio mal :/',e)
+    }
     
 
 }
