7f0e52ea0604ba7f27d06fcfc03ac171dc8f4dbb
rinel <rineliniguezsosa@gmail.com>
2023-02-06 16:30:43 -0600

    importando los estilos

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Firstapp } from './Firstapp';
 //import { App } from './Holamundo';
-
+import './estilos.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')) //REACT 18 YA  NO ES POSIBLE HACER EL RENDER DIRECTAMENTE
