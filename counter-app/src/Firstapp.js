2efe8c8394b817146b760bca06c17095642bf36b
rinel <rineliniguezsosa@gmail.com>
2023-02-06 16:19:16 -0600

    añadiendo un objeto de datos a la funcion Fisrtapp.js

--- counter-app/src/Firstapp.js
+++ counter-app/src/Firstapp.js
@@ -1,9 +1,14 @@
 
 
 export function Firstapp() {
+  const obj = {
+    name:'rinel',
+    email:'rineliniguezsosa@gmail.com'
+  }
   return (
     <>
         <h1>Rinel</h1>
+        <p>{JSON.stringify(obj)}</p>
     </>
   )
 }
