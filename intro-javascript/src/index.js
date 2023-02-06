0c80a2c7721b0d0cfe63a1d460bfe9bd675913aa
rinel <rineliniguezsosa@gmail.com>
2023-02-05 23:38:26 -0600

    llamando a la funcion findhero y resolviendo la promesa pasandole el heroe

--- intro-javascript/src/index.js
+++ intro-javascript/src/index.js
@@ -1,7 +1,8 @@
 import { findhero } from "./bases/08-import-export";
 
 const promesa = new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        
+    setTimeout(() => {  
+        const myhero = findhero(2);
+        resolve(myhero)
     },2000);
 })
