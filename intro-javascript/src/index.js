aa3e754d42cbc8eb43637113736d5011fad75d92
rinel <rineliniguezsosa@gmail.com>
2023-02-05 21:14:27 -0600

    buscando al heroe mediante el metodo find e imprimiendo en consola

--- intro-javascript/src/index.js
+++ intro-javascript/src/index.js
@@ -1,5 +1,8 @@
 import { heroes } from './data/heroes'
 
-const findhero = () => {
-    
+const findhero = (id) => {
+    return heroes.find((heroid) => heroid.id === id)
 }
+
+const datos = findhero(5);
+console.log(datos)
