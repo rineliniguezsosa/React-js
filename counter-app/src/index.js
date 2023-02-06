73cd5e39b3ca7d7578a90bc912695c043480d365
rinel <rineliniguezsosa@gmail.com>
2023-02-06 15:23:41 -0600

    eliminando la funcion App

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <>
-            <h1>Hola mundo</h1>
-        </>
-    );
-}
+
 
 ReactDOM.render(<App></App>,document.getElementById('root'))
 
