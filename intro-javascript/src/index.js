f04e6c784a21edba7976536afa90a989e071e82f
rinel <rineliniguezsosa@gmail.com>
2023-02-06 11:31:52 -0600

    realizando la peticion asincrona hacia el api giphy

--- intro-javascript/src/index.js
+++ intro-javascript/src/index.js
@@ -1,5 +1,10 @@
 
 const getImage = async() =>{
+    const apikey = 'F8OVeoJwy53KS0TI8S4VBdpmemu9QrXx';
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}`;
+    const resp = await fetch(url);
+    console.log(resp)
+    
 
 }
 
