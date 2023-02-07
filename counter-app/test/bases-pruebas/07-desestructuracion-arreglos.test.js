67fbef3733cec6f057ad4a8ecae163c976d60d49
rinel <rineliniguezsosa@gmail.com>
2023-02-07 13:58:06 -0600

    importando la funcion returnarr

--- counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
+++ counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
@@ -1,5 +1,8 @@
+import { returnarr } from "../../src/bases/07-desestructuracion-arreglos"
+
+
 describe('pruebas en el archivo 07-desestructuracion-arreglos.js', () => { 
     test('la funcion returnarr debe devolver un arreglo', () => { 
-        
+
      })
 })
