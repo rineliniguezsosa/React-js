802f0fd21212c77ff99d3ce98dd2a7d2f0462b04
rinel <rineliniguezsosa@gmail.com>
2023-02-06 20:48:27 -0600

    importando el funcional component CounterApp en el index.js

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Firstapp } from './Firstapp';
 //import { App } from './Holamundo';
 import './estilos.css'
+import { CounterApp } from './CounterApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')) //REACT 18 YA  NO ES POSIBLE HACER EL RENDER DIRECTAMENTE
