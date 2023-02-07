623156753dbdb25cd20892ba1ad494023621e1b2
rinel <rineliniguezsosa@gmail.com>
2023-02-07 14:39:22 -0600

    aplicando la prueba

--- counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
+++ counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
@@ -3,6 +3,9 @@ import { returnarr } from "../../src/bases/07-desestructuracion-arreglos"
 
 describe('pruebas en el archivo 07-desestructuracion-arreglos.js', () => { 
     test('la funcion returnarr debe devolver un string y un boolean', () => { 
-        
+        const [num,boo] = returnarr()
+    
+        expect(num).toEqual('133')
+        expect(boo).toEqual(true)
      })
 })
