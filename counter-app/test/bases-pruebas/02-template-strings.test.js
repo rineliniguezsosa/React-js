da9a3494f5b71509bf61aa69db97f22b2886094f
rinel <rineliniguezsosa@gmail.com>
2023-02-07 13:18:32 -0600

    definimos el expect toBe para que el valor devuelto por la funcion getsaludo corresponda o sea igual al Hola rinel

--- counter-app/test/bases-pruebas/02-template-strings.test.js
+++ counter-app/test/bases-pruebas/02-template-strings.test.js
@@ -5,5 +5,6 @@ describe('pruebas en el fichero 02-template-strings.js', () => {
     test('la funcion getsaludo debe de retornar hola rinel',()=>{
         const nombre = 'rinel';
         const msg = getsaludo(nombre)
+        expect(msg).toBe(`Hola ${nombre}`)
     })
 });
