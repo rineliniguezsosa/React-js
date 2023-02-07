562229e90af7b7b00f1ccd5d843806596246ca3c
rinel <rineliniguezsosa@gmail.com>
2023-02-07 13:35:55 -0600

    definiendo el objeto por devolucion de la funcion getusuario

--- counter-app/test/bases-pruebas/05-funciones.test.js
+++ counter-app/test/bases-pruebas/05-funciones.test.js
@@ -1,5 +1,8 @@
 describe('pruebas en el archivo 05-funciones.js', () => {
     test('la funcion getusuario debe de retornar un objeto',()=>{
-
+        const user =  {
+            uid:'123',
+            username:nombre
+        }
     })
 });
