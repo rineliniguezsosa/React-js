3fb6ca40abed18ff4efed61a896a7392a73972e4
rinel <rineliniguezsosa@gmail.com>
2023-02-07 13:43:56 -0600

    modificando la lectura de la variable rinel y esperando que el retorno de la funcion getusuario sea igual a el objeto user

--- counter-app/test/bases-pruebas/05-funciones.test.js
+++ counter-app/test/bases-pruebas/05-funciones.test.js
@@ -3,11 +3,14 @@ import { getusuario } from "../../src/bases/05-funciones";
 
 describe('pruebas en el archivo 05-funciones.js', () => {
     test('la funcion getusuario debe de retornar un objeto',()=>{
+        const nombre = 'rinel';
         const user =  {
             uid:'123',
             username:nombre
         }
-        const nombre = 'rinel';
+        
         const result = getusuario(nombre)
+
+        expect(result).toEqual(user);
     })
 });
