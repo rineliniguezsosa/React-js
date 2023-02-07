ef2437d2c1a9b5d181ca8afa194b0d9b7c534fa0
rinel <rineliniguezsosa@gmail.com>
2023-02-06 20:54:31 -0600

    definiendo las propTypes del funcional component CounterApp

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types'
 
-export function CounterApp() {
+export function CounterApp({value}) {
   return (
     <>
         <h1>CounterApp</h1>
         <h2>{value}</h2>    
     </>
   )
+}
+
+CounterApp.propTypes = {
+    value:PropTypes.number,
 }
