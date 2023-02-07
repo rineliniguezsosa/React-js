6307db066ea931251c555ec0dfa957be6a9f3b1f
rinel <rineliniguezsosa@gmail.com>
2023-02-07 17:51:28 -0600

    resolviendo la promesa y igualando el objeto extraido por el objeto extraido en el array de objetos,finalizando la prueba mediante el done

--- counter-app/test/bases-pruebas/09-Promesas.test.js
+++ counter-app/test/bases-pruebas/09-Promesas.test.js
@@ -4,6 +4,15 @@ import { getheroebyid } from "../../src/bases/09-Promesas"
 describe('pruebas en el archivo 09-Promesas.js', () => { 
     test('getheroebyid debe de retonar un heroe',(done)=>{
         const id = 1;
-        const data = getheroebyid(id)
+        getheroebyid(id)
+            .then(hero => {
+                expect(hero).toEqual({ 
+                    id: 1, 
+                    name: 'Batman', 
+                    owner: 'DC'
+                })
+                done();
+        })
+        
     })
  })
