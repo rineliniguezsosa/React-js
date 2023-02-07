7a71852bd2f2dc1261135d35d0b75f50e78e4c23
rinel <rineliniguezsosa@gmail.com>
2023-02-07 15:24:42 -0600

    aplicando la prueba esperando que el valor del result coincida con el objeto devuelto

--- counter-app/test/bases-pruebas/08-import-export.test.js
+++ counter-app/test/bases-pruebas/08-import-export.test.js
@@ -3,6 +3,8 @@ import { findhero } from "../../src/bases/08-import-export"
 
 describe('aplicando pruebas al archivo 08-import-export.js', () => { 
     test('el metodo findhero debe de retonar un id del heroe', () => {
-
+        const num = 5;
+        const result = findhero(num)
+        expect(result).toEqual({ id: 5, name: 'Wolverine', owner: 'Marvel' })
      })
  })
