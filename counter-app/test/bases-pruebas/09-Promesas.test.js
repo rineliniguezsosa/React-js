e308c233a9638cd302ff46eaac20a85f3de8d05d
rinel <rineliniguezsosa@gmail.com>
2023-02-07 16:57:16 -0600

    creando un id para pasarlo por argumento

--- counter-app/test/bases-pruebas/09-Promesas.test.js
+++ counter-app/test/bases-pruebas/09-Promesas.test.js
@@ -3,6 +3,6 @@ import { getheroebyid } from "../../src/bases/09-Promesas"
 
 describe('pruebas en el archivo 09-Promesas.js', () => { 
     test('getheroebyid debe de retonar un heroe',()=>{
-
+        const id = 1;
     })
  })
