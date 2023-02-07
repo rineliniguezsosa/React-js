a0cb2b96c5bd6509635fbdfb2fe2e52713571f24
rinel <rineliniguezsosa@gmail.com>
2023-02-07 12:53:50 -0600

    importando la funcion getsaludo

--- counter-app/test/bases-pruebas/02-template-strings.test.js
+++ counter-app/test/bases-pruebas/02-template-strings.test.js
@@ -1,5 +1,8 @@
+import { getsaludo } from "../../src/bases/02-template-strings";
+
+
 describe('pruebas en el fichero 02-template-strings.js', () => {
     test('la funcion getsaludo debe de retornar hola rinel',()=>{
-        
+
     })
 });
