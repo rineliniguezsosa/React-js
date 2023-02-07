16918e508de53af65edbdef87d5ca10a03b88ee1
rinel <rineliniguezsosa@gmail.com>
2023-02-06 20:52:09 -0600

    comentando la importacion del Firstapp

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Firstapp } from './Firstapp';
+//import { Firstapp } from './Firstapp';
 //import { App } from './Holamundo';
 import './estilos.css'
 import { CounterApp } from './CounterApp';
