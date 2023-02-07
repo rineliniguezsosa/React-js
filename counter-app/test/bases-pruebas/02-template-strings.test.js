fdbad546b03062cd181597969f94a361fabd1ed6
rinel <rineliniguezsosa@gmail.com>
2023-02-07 12:54:55 -0600

    creando la varibale nombre de tipo string con el valor de rinel

--- counter-app/test/bases-pruebas/02-template-strings.test.js
+++ counter-app/test/bases-pruebas/02-template-strings.test.js
@@ -3,6 +3,6 @@ import { getsaludo } from "../../src/bases/02-template-strings";
 
 describe('pruebas en el fichero 02-template-strings.js', () => {
     test('la funcion getsaludo debe de retornar hola rinel',()=>{
-
+        const nombre = 'rinel';
     })
 });
