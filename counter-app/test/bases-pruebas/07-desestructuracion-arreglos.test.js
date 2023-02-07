7f142357e59e530424f42737dfde166bfd3bb703
rinel <rineliniguezsosa@gmail.com>
2023-02-07 14:19:13 -0600

    modificando la descripcion de la prueba

--- counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
+++ counter-app/test/bases-pruebas/07-desestructuracion-arreglos.test.js
@@ -2,7 +2,7 @@ import { returnarr } from "../../src/bases/07-desestructuracion-arreglos"
 
 
 describe('pruebas en el archivo 07-desestructuracion-arreglos.js', () => { 
-    test('la funcion returnarr debe devolver un arreglo', () => { 
-
+    test('la funcion returnarr debe devolver un string y un boolean', () => { 
+        
      })
 })
