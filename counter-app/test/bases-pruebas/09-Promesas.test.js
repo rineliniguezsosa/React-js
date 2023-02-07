66fb55ff00d047802bd0380f496411b6e8364423
rinel <rineliniguezsosa@gmail.com>
2023-02-07 16:58:36 -0600

    llamando a la funcion getheroebyid y pasandole el argumento id

--- counter-app/test/bases-pruebas/09-Promesas.test.js
+++ counter-app/test/bases-pruebas/09-Promesas.test.js
@@ -4,5 +4,6 @@ import { getheroebyid } from "../../src/bases/09-Promesas"
 describe('pruebas en el archivo 09-Promesas.js', () => { 
     test('getheroebyid debe de retonar un heroe',()=>{
         const id = 1;
+        const data = getheroebyid(id)
     })
  })
