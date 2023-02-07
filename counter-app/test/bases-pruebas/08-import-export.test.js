c15460ce0c53f4286630ae364969b78c75c1fa4c
rinel <rineliniguezsosa@gmail.com>
2023-02-07 14:58:35 -0600

    importando la funcion findhero

--- counter-app/test/bases-pruebas/08-import-export.test.js
+++ counter-app/test/bases-pruebas/08-import-export.test.js
@@ -1,5 +1,8 @@
+import { findhero } from "../../src/bases/08-import-export"
+
+
 describe('aplicando pruebas al archivo 08-import-export.js', () => { 
     test('el metodo findhero debe de retonar un id del heroe', () => {
-        
+
      })
  })
