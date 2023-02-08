1df502e0c01d844086e671fc765d20cf660590fa
rinel <rineliniguezsosa@gmail.com>
2023-02-08 15:09:29 -0600

    importando fireEvent para la simulacion de ventos

--- counter-app/test/bases-pruebas/CounterApp.test.js
+++ counter-app/test/bases-pruebas/CounterApp.test.js
@@ -1,4 +1,4 @@
-import { render,screen } from "@testing-library/react"
+import { fireEvent, render,screen } from "@testing-library/react"
 import { CounterApp } from "../../src/CounterApp"
 
 describe('pruebas en el componente <CounterApp/>', () => { 
