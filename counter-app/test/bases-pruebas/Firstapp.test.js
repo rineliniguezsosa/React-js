5e19f80178443c8e6dc8693c8dc7bb0c0ba55827
rinel <rineliniguezsosa@gmail.com>
2023-02-08 10:47:26 -0600

    importando el componente Firstapp.js

--- counter-app/test/bases-pruebas/Firstapp.test.js
+++ counter-app/test/bases-pruebas/Firstapp.test.js
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react'
+import { Firstapp } from '../../src/Firstapp'
 
 describe('Pruebas en el componente <Firstapp>', () => { 
   test('Deberia de coincidir con el snapshot', () => { 
-    
+      
    })
  })
