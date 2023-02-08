7d9a742f489d48f9a2d9a67aafbb257b235a93b3
rinel <rineliniguezsosa@gmail.com>
2023-02-08 10:48:39 -0600

    renderizando el componente Firstapp

--- counter-app/test/bases-pruebas/Firstapp.test.js
+++ counter-app/test/bases-pruebas/Firstapp.test.js
@@ -3,6 +3,6 @@ import { Firstapp } from '../../src/Firstapp'
 
 describe('Pruebas en el componente <Firstapp>', () => { 
   test('Deberia de coincidir con el snapshot', () => { 
-      
+      render(<Firstapp></Firstapp>)
    })
  })
