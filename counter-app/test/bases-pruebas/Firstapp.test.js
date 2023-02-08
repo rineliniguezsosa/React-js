29267d64893ca84815e6886f7916cd4cc0fda058
rinel <rineliniguezsosa@gmail.com>
2023-02-08 12:40:20 -0600

    importando screen de testing-library/react

--- counter-app/test/bases-pruebas/Firstapp.test.js
+++ counter-app/test/bases-pruebas/Firstapp.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render,screen } from '@testing-library/react'
 import { Firstapp } from '../../src/Firstapp'
 
 describe('Pruebas en el componente <Firstapp>', () => { 
