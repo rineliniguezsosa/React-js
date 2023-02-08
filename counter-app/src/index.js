305ac2f9321180e81c5f2ad92826a2902360bbdf
rinel <rineliniguezsosa@gmail.com>
2023-02-08 14:20:36 -0600

    comentando la impirtacion de Firstapp y renderizado el componente CounterAPP

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Firstapp } from './Firstapp';
+//import { Firstapp } from './Firstapp';
 //import { App } from './Holamundo';
 import './estilos.css'
-//import { CounterApp } from './CounterApp';
+import { CounterApp } from './CounterApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')) //REACT 18 YA  NO ES POSIBLE HACER EL RENDER DIRECTAMENTE
-root.render(<Firstapp name="rinel" edad={27}></Firstapp>) //REACT 18
+root.render(<CounterApp value={100}></CounterApp>) //REACT 18
 
