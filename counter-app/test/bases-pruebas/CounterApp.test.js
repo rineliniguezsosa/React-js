7c528f060a5e82eaf1e9cd12d6c456f37707f6b0
rinel <rineliniguezsosa@gmail.com>
2023-02-08 14:25:48 -0600

    importando render de testing-library de react

--- counter-app/test/bases-pruebas/CounterApp.test.js
+++ counter-app/test/bases-pruebas/CounterApp.test.js
@@ -1,7 +1,8 @@
+import { render } from "@testing-library/react"
 import { CounterApp } from "../../src/CounterApp"
 
 describe('pruebas en el componente <CounterApp/>', () => { 
     test('Debe de hacer match con el snapshot',()=>{
-
+     
     })
 })
