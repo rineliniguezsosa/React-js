3d314199cb5915a024a79c8b8ad0b03cf783f013
rinel <rineliniguezsosa@gmail.com>
2023-02-08 14:27:09 -0600

    renderizando el componente CounterApp y desestructurando el elemento html container

--- counter-app/test/bases-pruebas/CounterApp.test.js
+++ counter-app/test/bases-pruebas/CounterApp.test.js
@@ -3,6 +3,6 @@ import { CounterApp } from "../../src/CounterApp"
 
 describe('pruebas en el componente <CounterApp/>', () => { 
     test('Debe de hacer match con el snapshot',()=>{
-     
+     const { container } = render(<CounterApp></CounterApp>)
     })
 })
