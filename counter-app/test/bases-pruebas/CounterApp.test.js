27027bf83cd78006eeecb56a3a85617a0155575c
rinel <rineliniguezsosa@gmail.com>
2023-02-08 14:31:58 -0600

    haciendo el march con el snapshot del componente CounterApp

--- counter-app/test/bases-pruebas/CounterApp.test.js
+++ counter-app/test/bases-pruebas/CounterApp.test.js
@@ -4,5 +4,6 @@ import { CounterApp } from "../../src/CounterApp"
 describe('pruebas en el componente <CounterApp/>', () => { 
     test('Debe de hacer match con el snapshot',()=>{
      const { container } = render(<CounterApp></CounterApp>)
+     expect(container).toMatchSnapshot();
     })
 })
