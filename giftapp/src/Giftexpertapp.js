6763d251891ef31eca97a2d382cbe6baa86ba0b8
rinel <rineliniguezsosa@gmail.com>
2023-02-08 17:18:14 -0600

    definiendo el hook useState donde el valor inicial del componente sera un array con el valor Gon de hunter x hunter

--- giftapp/src/Giftexpertapp.js
+++ giftapp/src/Giftexpertapp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 export function Giftexpertapp() {
+  const [categorias, setCategorias] = useState(['Gon'])
   return (
     <>
         <h1>Giftexpertapp</h1>
