5f2ab41ce0acc3dede8813f30f627041e80ba0de
rinel <rineliniguezsosa@gmail.com>
2023-02-07 20:01:58 -0600

    realizando la prueba cuando el id del heroe es alguno que no existe manejamos el error catch para la resolucion de errores de la promesa

--- counter-app/test/bases-pruebas/09-Promesas.test.js
+++ counter-app/test/bases-pruebas/09-Promesas.test.js
@@ -15,4 +15,13 @@ describe('pruebas en el archivo 09-Promesas.js', () => {
         })
         
     })
+    test('getheroebyid debe de retonar un error si heroe no existe',(done)=>{
+        const id = 100;
+        getheroebyid(id)
+            .catch(error => {
+                // eslint-disable-next-line jest/no-conditional-expect
+                expect(error).toBe('No existe el id')
+                done();
+        })
+    })
  })
