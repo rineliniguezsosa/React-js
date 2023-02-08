1c8a6bc77b84019185afb5e47b0a232a0863ef7f
rinel <rineliniguezsosa@gmail.com>
2023-02-08 14:38:19 -0600

    importando el modulo screen de testing-library/react

--- counter-app/test/bases-pruebas/CounterApp.test.js
+++ counter-app/test/bases-pruebas/CounterApp.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render,screen } from "@testing-library/react"
 import { CounterApp } from "../../src/CounterApp"
 
 describe('pruebas en el componente <CounterApp/>', () => { 
