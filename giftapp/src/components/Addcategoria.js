4f2b86600a40f6a914054159a5ee0f7c37e6d33e
rinel <rineliniguezsosa@gmail.com>
2023-02-09 11:30:49 -0600

    definiendo como valor inicial el inputvalue en la propiedad value del input text

--- giftapp/src/components/Addcategoria.js
+++ giftapp/src/components/Addcategoria.js
@@ -5,6 +5,6 @@ import { useState } from "react"
 export function Addcategoria() {
   const [inputvalue, setInputvalue] = useState('Gon')
   return (
-    <input type="text" placeholder="Buscar">Addcategoria</input>
+    <input type="text" placeholder="Buscar" value={inputvalue}>Addcategoria</input>
   )
 }
