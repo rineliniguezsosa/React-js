a039f313583fe583ce928f843ca84ce3c6d7050e
rinel <rineliniguezsosa@gmail.com>
2023-02-09 14:40:04 -0600

    obtenemos la prop setCategorias

--- giftapp/src/components/Addcategoria.js
+++ giftapp/src/components/Addcategoria.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 
 
-export function Addcategoria() {
+export function Addcategoria({setCategorias}) {
   const [inputvalue, setInputvalue] = useState('Gon')
 
   const inputchange = (event) =>{
