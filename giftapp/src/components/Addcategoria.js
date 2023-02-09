267a802bc4e316c65bb9b745351057aedeed0547
rinel <rineliniguezsosa@gmail.com>
2023-02-09 11:39:25 -0600

    creando la funcion de flecha inputchange

--- giftapp/src/components/Addcategoria.js
+++ giftapp/src/components/Addcategoria.js
@@ -4,6 +4,10 @@ import { useState } from "react"
 
 export function Addcategoria() {
   const [inputvalue, setInputvalue] = useState('Gon')
+
+  const inputchange = () =>{
+    
+  }
   return (
     <input type="text" placeholder="Buscar" value={inputvalue} onChange={inputchange}>Addcategoria</input>
   )
