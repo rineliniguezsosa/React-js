9f62656e11fc24f635add80008ebed764d104511
rinel <rineliniguezsosa@gmail.com>
2023-02-10 12:34:35 -0600

    corrigiendo la propiedad img por id

--- giftapp/src/helpers/Getgifs.js
+++ giftapp/src/helpers/Getgifs.js
@@ -2,7 +2,7 @@ export const getgift =  async(usercategory) =>{
     const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`);
     const {data} = await req.json();
     const gifs = data.map(img => ({
-        img:img.id,
+        id:img.id,
         titulo:img.title,
         url:img.images.downsized.url,
     }))
