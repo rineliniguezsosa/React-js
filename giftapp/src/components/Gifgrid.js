3534026ea57579e22cf95367fd03abdec8c3672a
rinel <rineliniguezsosa@gmail.com>
2023-02-09 20:36:56 -0600

    creando la variable req que espera la peticion http de la api de los gifs

--- giftapp/src/components/Gifgrid.js
+++ giftapp/src/components/Gifgrid.js
@@ -2,7 +2,8 @@
 
 export function Gifgrid({usercategory}) {
   const getgift =  async() =>{
-  
+    const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`)
+    
   }
   return (
     <>
