95693c4ee63e6cc6309e3958327dc243829eff48
rinel <rineliniguezsosa@gmail.com>
2023-02-10 13:27:46 -0600

    pegando la logica del Gifgrid

--- giftapp/src/hooks/useFetchgifs.js
+++ giftapp/src/hooks/useFetchgifs.js
@@ -2,4 +2,13 @@
 
 export function useFetchgifs() {
   
+  const [images, setImages] = useState([])
+  const getImages = async(usercategory) => {
+    const img = await getgift(usercategory)
+    setImages(img)
+  }
+  useEffect(() => {
+    //getgift(usercategory)
+    getImages(usercategory)
+  }, [usercategory])
 }
