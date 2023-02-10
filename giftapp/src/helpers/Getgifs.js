d831724acd4497dc5cf4eae5427565fbbe00ff31
rinel <rineliniguezsosa@gmail.com>
2023-02-09 21:17:31 -0600

    agregando el parametro usercategory a la funcion getgift

--- giftapp/src/helpers/Getgifs.js
+++ giftapp/src/helpers/Getgifs.js
@@ -1,4 +1,4 @@
-export const getgift =  async() =>{
+export const getgift =  async(usercategory) =>{
     const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`);
     const {data} = await req.json();
     const gifs = data.map(img => ({
