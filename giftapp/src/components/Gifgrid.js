0905d45319f58e292f735ed0272d74d0731de979
rinel <rineliniguezsosa@gmail.com>
2023-02-10 13:30:54 -0600

    comentando multiples lineas y aplicando el customhook el cual de la funcion useFetchgifs se desestructura el retorno

--- giftapp/src/components/Gifgrid.js
+++ giftapp/src/components/Gifgrid.js
@@ -1,18 +1,21 @@
-import { getgift } from "../helpers/Getgifs"
-import React, { useState,useEffect } from 'react';
+//import { getgift } from "../helpers/Getgifs"
+//import React, { useState,useEffect } from 'react';
 import { Griditem } from "./Griditem";
+import { useFetchgifs } from "../hooks/useFetchgifs";
 
 
 export function Gifgrid({usercategory}) {
-  const [images, setImages] = useState([])
-  const getImages = async(usercategory) => {
-    const img = await getgift(usercategory)
-    setImages(img)
-  }
-  useEffect(() => {
-    //getgift(usercategory)
-    getImages(usercategory)
-  }, [usercategory])
+
+  const {images,cargando} = useFetchgifs()
+  // const [images, setImages] = useState([])
+  // const getImages = async(usercategory) => {
+  //   const img = await getgift(usercategory)
+  //   setImages(img)
+  // }
+  // useEffect(() => {
+  //   //getgift(usercategory)
+  //   getImages(usercategory)
+  // }, [usercategory])
   
   return (
     <>
