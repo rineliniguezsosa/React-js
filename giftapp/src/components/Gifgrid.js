af4991b0c3fef7315cae7cba2d42b6b94d8fb07a
rinel <rineliniguezsosa@gmail.com>
2023-02-09 21:09:29 -0600

    funcion asincrona que devuelve los gifs

--- giftapp/src/components/Gifgrid.js
+++ giftapp/src/components/Gifgrid.js
@@ -2,10 +2,16 @@
 
 export function Gifgrid({usercategory}) {
   const getgift =  async() =>{
-    const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`)
-    const resp = await req.json()
-    
+    const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`);
+    const {data} = await req.json();
+    const gifs = data.map(img => ({
+        img:img.id,
+        titulo:img.title,
+        url:img.images.downsized.url,
+    }))
+    return gifs;
   }
+  getgift()
   return (
     <>
         <h1>{usercategory}</h1>
