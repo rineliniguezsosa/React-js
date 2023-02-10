8c93ff2696ff0efb4c0de9ed89a37191f5f485f2
rinel <rineliniguezsosa@gmail.com>
2023-02-09 21:14:12 -0600

    eliminando el contenido de la funcion asincrona getgift

--- giftapp/src/components/Gifgrid.js
+++ giftapp/src/components/Gifgrid.js
@@ -1,16 +1,7 @@
 
 
 export function Gifgrid({usercategory}) {
-  const getgift =  async() =>{
-    const req = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=DJKw39bBPwS3pPTtIpZnriojFdIDYaEC&q=${usercategory}&limit=15`);
-    const {data} = await req.json();
-    const gifs = data.map(img => ({
-        img:img.id,
-        titulo:img.title,
-        url:img.images.downsized.url,
-    }))
-    return gifs;
-  }
+  
   getgift()
   return (
     <>
