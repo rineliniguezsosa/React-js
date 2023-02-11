ecf1fc5c338934c604b4d8c687430c66cc6fe843
rinel <rineliniguezsosa@gmail.com>
2023-02-11 17:13:38 -0600

    importando el componente Addcategoria

--- giftapp/test/components/Addcategoria.test.js
+++ giftapp/test/components/Addcategoria.test.js
@@ -1,4 +1,6 @@
 import { render } from '@testing-library/react'
+import { Addcategoria } from '../../src/components/Addcategoria';
+
 
 describe('Pruebas en el componente <Addcategoria/>', () => {
   test('Debe de cambiar el valor de la caja de texto', () => {
