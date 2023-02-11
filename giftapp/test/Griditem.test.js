4084baf2232f4171cd91df58557cce3accfd7d6f
rinel <rineliniguezsosa@gmail.com>
2023-02-10 19:14:43 -0600

    corrigiendo la importacion del Griditem

--- giftapp/test/Griditem.test.js
+++ giftapp/test/Griditem.test.js
@@ -1,4 +1,4 @@
-const { Griditem } = require("../src/components/Griditem");
+import { Griditem } from '../src/components/Griditem'
 
 describe('Pruebas en el componente <Griditem/>', () => { 
     test('Debe de coincidir con el snap shot', () => {
