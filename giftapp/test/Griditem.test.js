5557f67dc81b4fea0c0cc243c73977bae11ca25d
rinel <rineliniguezsosa@gmail.com>
2023-02-10 19:19:01 -0600

    renderizando o montando el componente Griditem

--- giftapp/test/Griditem.test.js
+++ giftapp/test/Griditem.test.js
@@ -3,6 +3,6 @@ import { render } from '@testing-library/react'
 
 describe('Pruebas en el componente <Griditem/>', () => { 
     test('Debe de coincidir con el snap shot', () => {
-      
+      const { container } = render(<Griditem/>)
     });
  })
