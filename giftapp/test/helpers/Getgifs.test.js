70516bac3ce1378b50e55888b65c5366374a514d
rinel <rineliniguezsosa@gmail.com>
2023-02-11 14:58:56 -0600

    resolviendo la respuesta sea igual al array de objetos

--- giftapp/test/helpers/Getgifs.test.js
+++ giftapp/test/helpers/Getgifs.test.js
@@ -2,6 +2,13 @@ import { getgift } from "../../src/helpers/Getgifs";
 
 describe('Pruebas en la funcion asincrona getgift()', () => {
     test('Debe de retornar un arreglo de gifs',async () => {
-
+        const gifs = await getgift('batman')
+       // console.log(gifs[0])
+       // expect(gifs.length).toBeGreaterThan(0);
+        expect(gifs[0]).toEqual({
+            id:expect.any(String),
+            titulo:expect.any(String),
+            url:expect.any(String)
+        })
     });
 });
