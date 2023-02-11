af909a0f9ca8c4c4070b5c76658623b4ed84e339
rinel <rineliniguezsosa@gmail.com>
2023-02-10 19:56:21 -0600

    pasando la prop titulo y url a los componentes Griditem

--- giftapp/test/Griditem.test.js
+++ giftapp/test/Griditem.test.js
@@ -2,17 +2,17 @@ import { Griditem } from '../src/components/Griditem'
 import { render,screen } from '@testing-library/react'
 
 describe('Pruebas en el componente <Griditem/>', () => { 
-  
+
     const url = "https://giphy.com/gifs/gon-u4dQ8BMugUYp2";
     const titutlo = "gon GIF";
 
     test('Debe de coincidir con el snap shot', () => {
-      const { container } = render(<Griditem url="https://giphy.com/gifs/gon-u4dQ8BMugUYp2" titulo="gon GIF"/>)
+      const { container } = render(<Griditem url={url} titulo={titulo}/>)
 
       expect(container).toMatchSnapshot();
     });
 
     test('Debe de mostrar el url y al alt indicado', () => {
-      render(<Griditem url="https://giphy.com/gifs/gon-u4dQ8BMugUYp2" titulo="gon GIF"/>)
+      render(<Griditem url={url} titulo={titulo}/>)
     });
  })
