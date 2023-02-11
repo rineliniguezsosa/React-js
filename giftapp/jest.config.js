eb1c7fb8aed24f4fa3c725d331b21535cdea74ee
rinel <rineliniguezsosa@gmail.com>
2023-02-11 14:39:36 -0600

    configurando setupFiles en donde definimos el path del archivo jest.setup el cual contiene la importacion del los modulos instalados para las pruebas

--- giftapp/jest.config.js
+++ giftapp/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom'
+    testEnvironment: 'jest-environment-jsdom',
     //especificamos el testEnviroment para el entorno de prueba
+    setupFiles:['./jest.setup.js']
+    //descripcion setupFiles Una lista de rutas a módulos que ejecutan algún código para configurar o configurar el entorno de prueba.
 }
