e08d8089c2f624f1e77255c30f0fe18fd007a434
rinel <rineliniguezsosa@gmail.com>
2023-02-11 17:14:15 -0600

    renderizando el componente Addcategoria

--- giftapp/test/components/Addcategoria.test.js
+++ giftapp/test/components/Addcategoria.test.js
@@ -4,6 +4,6 @@ import { Addcategoria } from '../../src/components/Addcategoria';
 
 describe('Pruebas en el componente <Addcategoria/>', () => {
   test('Debe de cambiar el valor de la caja de texto', () => {
-      
+      render(<Addcategoria></Addcategoria>)
   });
 });
