80ce8901f4d449f98de833f327adc23a45d34379
rinel <rineliniguezsosa@gmail.com>
2023-02-11 13:46:52 -0600

    moviendo el archivo Griditem.test.js a la carpeta components

--- giftapp/test/components/Griditem.test.js
+++ giftapp/test/components/Griditem.test.js
@@ -1,4 +1,4 @@
-import { Griditem } from '../src/components/Griditem'
+import { Griditem } from '../../src/components/Griditem'
 import { render,screen } from '@testing-library/react'
 
 describe('Pruebas en el componente <Griditem/>', () => { 
