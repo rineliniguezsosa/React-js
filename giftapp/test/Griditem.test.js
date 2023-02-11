831e1ea6b6ce8f34f5db8b3856bfa7af405461b9
rinel <rineliniguezsosa@gmail.com>
2023-02-10 19:39:51 -0600

    añadiendo las props y corrigiendo el toMatchSnapshot

--- giftapp/test/Griditem.test.js
+++ giftapp/test/Griditem.test.js
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react'
 
 describe('Pruebas en el componente <Griditem/>', () => { 
     test('Debe de coincidir con el snap shot', () => {
-      const { container } = render(<Griditem/>)
+      const { container } = render(<Griditem url="https://giphy.com/gifs/gon-u4dQ8BMugUYp2" titulo="gon GIF"/>)
 
-      expect(container).toMatchInlineSnapshot();
+      expect(container).toMatchSnapshot();
     });
  })
