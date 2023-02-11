3fafbcc55cc317df8b279c4e523108fb0e83e74c
rinel <rineliniguezsosa@gmail.com>
2023-02-11 17:12:22 -0600

    importando render de testing/library-react

--- giftapp/test/components/Addcategoria.test.js
+++ giftapp/test/components/Addcategoria.test.js
@@ -1,5 +1,7 @@
+import { render } from '@testing-library/react'
+
 describe('Pruebas en el componente <Addcategoria/>', () => {
   test('Debe de cambiar el valor de la caja de texto', () => {
-    
+      
   });
 });
