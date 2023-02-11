65cefbb8752cb0da604f95442374ae6e11f3e84d
rinel <rineliniguezsosa@gmail.com>
2023-02-11 13:54:31 -0600

    importando la funcion asincrona getgift

--- giftapp/test/helpers/Getgifs.test.js
+++ giftapp/test/helpers/Getgifs.test.js
@@ -1,5 +1,7 @@
+import { getgift } from "../../src/helpers/Getgifs";
+
 describe('Pruebas en la funcion asincrona getgift()', () => {
     test('Debe de retornar un arreglo de gifs',async () => {
-        
+
     });
 });
