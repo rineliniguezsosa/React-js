26ed8417a4388a59859859b512c241e96c762f3b
rinel <rineliniguezsosa@gmail.com>
2023-02-11 19:01:45 -0600

    buscamos el elemento textbox el cual sera el elemento html input

--- giftapp/test/components/Addcategoria.test.js
+++ giftapp/test/components/Addcategoria.test.js
@@ -1,9 +1,11 @@
-import { render,screen } from '@testing-library/react'
+import { fireEvent, render,screen } from '@testing-library/react'
 import { Addcategoria } from '../../src/components/Addcategoria';
 
 
 describe('Pruebas en el componente <Addcategoria/>', () => {
   test('Debe de cambiar el valor de la caja de texto', () => {
       render(<Addcategoria onNewCategorias={()=> {}}></Addcategoria>)
+      const input = screen.getByRole('textbox') //textbox --> html input 
+      
   });
 });
