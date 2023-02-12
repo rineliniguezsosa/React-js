956cc559713b790d9f44d532e1378d1a1eab0d7a
rinel <rineliniguezsosa@gmail.com>
2023-02-11 18:28:50 -0600

    importando screen y agregando la prop onNewCategorias el cual devuelve una funcion

--- giftapp/test/components/Addcategoria.test.js
+++ giftapp/test/components/Addcategoria.test.js
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react'
+import { render,screen } from '@testing-library/react'
 import { Addcategoria } from '../../src/components/Addcategoria';
 
 
 describe('Pruebas en el componente <Addcategoria/>', () => {
   test('Debe de cambiar el valor de la caja de texto', () => {
-      render(<Addcategoria></Addcategoria>)
+      render(<Addcategoria onNewCategorias={()=> {}}></Addcategoria>)
   });
 });
