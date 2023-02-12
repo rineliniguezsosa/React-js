3a71818aeaa00684363eb9e75f97256b7433be04
rinel <rineliniguezsosa@gmail.com>
2023-02-12 15:47:12 -0600

    importando screen  de testing-library/react

--- giftapp/test/components/Gifgrid.test.js
+++ giftapp/test/components/Gifgrid.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render,screen } from '@testing-library/react'
 import { Gifgrid } from '../../src/components/Gifgrid';
 
 describe('Prueba en el componente <Gifgrid/>', () => {
