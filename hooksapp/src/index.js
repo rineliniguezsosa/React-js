e73055ed65449b79fd7c059bd6d8791408ccc27e
rinel <rineliniguezsosa@gmail.com>
2023-02-13 10:04:19 -0600

    importando counterapp,comentando Hookapp y renderizando el Counterapp en el dom

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Hookapp } from './components/Hookapp';
+import { Counterapp } from './01-useState/Counterapp';
+//import { Hookapp } from './components/Hookapp';
 import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Hookapp/>
+        <Counterapp></Counterapp>
     </React.StrictMode>
 )
