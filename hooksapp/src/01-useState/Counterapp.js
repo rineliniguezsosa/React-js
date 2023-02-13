6858c7054979262b29f40eb5e27f254cf24c3bb5
rinel <rineliniguezsosa@gmail.com>
2023-02-13 10:52:33 -0600

    cambiando la desestructuracion por el objeto cuenta y utilizando el operador spread para no perder los valores del objeto y incrementando el valor counter1

--- hooksapp/src/01-useState/Counterapp.js
+++ hooksapp/src/01-useState/Counterapp.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
 
 export const Counterapp = () => {
-  const [cuenta, setCuenta] = useState(10)
+  const [cuenta, setCuenta] = useState({
+    counter1:10,
+    counter2:20,
+    counter3:30,
+  })
+  const { counter1,counter2,counter3} = cuenta
   return (
     <>
     <span>Counterapp</span>
-    <span>Cuenta: {cuenta}</span>
-    <button onClick={ ()=> setCuenta(cuenta+1)}>+1</button>
+    <h1>counter1 {counter1}</h1>
+    <h1>counter2 {counter2} </h1>
+    <h1>counter3 {counter3}</h1>
+
+
+    {/* <span>Cuenta: {cuenta}</span> */}
+    <button onClick={ ()=> setCuenta({
+        ...cuenta,
+        counter1:counter1+1,
+    })}>+1</button>
     </>
   )
 }
