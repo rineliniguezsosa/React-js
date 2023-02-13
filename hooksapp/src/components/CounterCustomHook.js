b545c22a2e5299ee0a00fa328394a8beefe073c1
rinel <rineliniguezsosa@gmail.com>
2023-02-13 12:44:40 -0600

    corrigiendo el orden de la desestructuracion de los metodos y agregando metodos onClick para ejecutar cada funcion unica

--- hooksapp/src/components/CounterCustomHook.js
+++ hooksapp/src/components/CounterCustomHook.js
@@ -4,15 +4,15 @@ import { useCounter } from "../hooks/useCounter"
 export const CounterCustomHook = () => {
   const {value,actions} = useCounter(0)
 
-  const [ increment,reset,decrement ] = actions
+  const [ increment,decrement,reset ] = actions
   return (
     <>
         <span>contador con hook</span>
         <hr></hr>
         <span>{value}</span>
         <button onClick={()=> increment()} className="btn btn-primary">+1</button>
-        <button className="btn btn-primary">reset</button>
-        <button className="btn btn-primary">-1</button>
+        <button onClick={()=> reset()} className="btn btn-primary">reset</button>
+        <button onClick={()=> decrement()} className="btn btn-primary">-1</button>
     </>
   )
 }
