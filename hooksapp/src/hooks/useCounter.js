b9b40296086f153275948a3eaae1dbd0fed06431
rinel <rineliniguezsosa@gmail.com>
2023-02-13 11:55:09 -0600

    creando arrow function increment

--- hooksapp/src/hooks/useCounter.js
+++ hooksapp/src/hooks/useCounter.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 
 export const useCounter = (valorinicial) => {
-  const [value, setValue] = useState(valorinicial)  
+  const [value, setValue] = useState(valorinicial) 
+  
+  const increment = () =>{
+    
+  }
   return {
     value,
   }
