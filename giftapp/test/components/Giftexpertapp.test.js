39f5ea82e244ca8cba3d97e9bdad521607dd0395
rinel <rineliniguezsosa@gmail.com>
2023-02-12 21:52:32 -0600

    eliminando el espacio en blanco

--- giftapp/test/components/Giftexpertapp.test.js
+++ giftapp/test/components/Giftexpertapp.test.js
@@ -2,7 +2,7 @@ import { render,screen } from "@testing-library/react";
 import { Giftexpertapp } from "../../src/Giftexpertapp";
 
 describe('Pruebas en el componente <Giftexpertapp/>', () => {
-  test('El state inicial debe ser gon ', () => {
+  test('El state inicial debe ser gon', () => {
   
   }); 
 });
