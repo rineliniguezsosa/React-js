295ad24d6ff0d63f5d099eb32f5642e75a786fe8
rinel <rineliniguezsosa@gmail.com>
2023-02-12 22:00:14 -0600

    tomando el snapshot del container y añadiendo un salto de linea

--- giftapp/test/components/Giftexpertapp.test.js
+++ giftapp/test/components/Giftexpertapp.test.js
@@ -4,7 +4,9 @@ import { Giftexpertapp } from "../../src/Giftexpertapp";
 describe('Pruebas en el componente <Giftexpertapp/>', () => {
   test('Debe de coincidir con el snapshot', () => {
     const { container } = render(<Giftexpertapp></Giftexpertapp>)
+    expect(container).toMatchSnapshot();
   });
+  
   test('El state inicial debe ser gon', () => {
     render(<Giftexpertapp/>)
 
