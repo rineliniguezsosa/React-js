70add81be4c1ac8de98e1911d3bb058833cb872c
rinel <rineliniguezsosa@gmail.com>
2023-02-13 09:29:10 -0600

    importando los estilos.css en el index.js

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Hookapp } from './components/Hookapp';
+import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
