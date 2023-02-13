85b9c14e0c5feaaf2d49d5e78a19f63bc2d5f72b
rinel <rineliniguezsosa@gmail.com>
2023-02-12 21:30:23 -0600

    importando screen de testing-library/react

--- giftapp/test/components/Giftexpertapp.test.js
+++ giftapp/test/components/Giftexpertapp.test.js
@@ -1,7 +1,6 @@
-import { render } from "@testing-library/react";
+import { render,screen } from "@testing-library/react";
 import { Giftexpertapp } from "../../src/Giftexpertapp";
 
 describe('Pruebas en el componente <Giftexpertapp/>', () => {
   render(<Giftexpertapp></Giftexpertapp>)
-  
 });
