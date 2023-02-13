49c75facb8b806fcd52c63c9f153aaf4a2cf2394
rinel <rineliniguezsosa@gmail.com>
2023-02-13 13:00:27 -0600

    creando un nuevo input de tipo email

--- hooksapp/src/02-useEffect/SimpleForm.js
+++ hooksapp/src/02-useEffect/SimpleForm.js
@@ -5,6 +5,8 @@ export const SimpleForm = () => {
     <span>SimpleForm</span>
     <hr></hr>
     <input type="text" placeholder="username" name="username" className="form-control"></input>
+
+    <input type="email" placeholder="rineliniguezsosa@gmail.com" name="email" className="form-control mt-2"></input>
     </>
   )
 }
