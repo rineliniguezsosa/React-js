46bd21c901cc9a83ebf1e887163340c4daafe4b5
rinel <rineliniguezsosa@gmail.com>
2023-02-12 21:52:02 -0600

    definiendo el bloque test con la sig descripcion: El state inicial debe ser gon

--- giftapp/test/components/Giftexpertapp.test.js
+++ giftapp/test/components/Giftexpertapp.test.js
@@ -2,5 +2,7 @@ import { render,screen } from "@testing-library/react";
 import { Giftexpertapp } from "../../src/Giftexpertapp";
 
 describe('Pruebas en el componente <Giftexpertapp/>', () => {
-  render(<Giftexpertapp></Giftexpertapp>)
+  test('El state inicial debe ser gon ', () => {
+  
+  }); 
 });
