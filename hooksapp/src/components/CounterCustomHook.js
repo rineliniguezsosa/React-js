23d0d4d1d1a3695335d48c0fd199eb4a0f28db99
rinel <rineliniguezsosa@gmail.com>
2023-02-13 11:48:16 -0600

    desestructurando el useCounter y renderizando el value en el html

--- hooksapp/src/components/CounterCustomHook.js
+++ hooksapp/src/components/CounterCustomHook.js
@@ -2,10 +2,12 @@ import { useCounter } from "../hooks/useCounter"
 
 
 export const CounterCustomHook = () => {
+  const {value} = useCounter(0)
   return (
     <>
         <span>contador con hook</span>
         <hr></hr>
+        <span>{value}</span>
         <button className="btn btn-primary">+1</button>
         <button className="btn btn-primary">reset</button>
         <button className="btn btn-primary">-1</button>
