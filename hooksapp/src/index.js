183cd710189700fc50e2492bd0bdc3e75eb4f834
rinel <rineliniguezsosa@gmail.com>
2023-02-13 11:47:10 -0600

    importando CounterCustomHook ,comantando el componente Counterapp y renderizando el componente CounterCustomHook

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Counterapp } from './01-useState/Counterapp';
+//import { Counterapp } from './01-useState/Counterapp';
+import { CounterCustomHook } from './components/CounterCustomHook';
 //import { Hookapp } from './components/Hookapp';
 import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Counterapp></Counterapp>
+        <CounterCustomHook></CounterCustomHook>
     </React.StrictMode>
 )
