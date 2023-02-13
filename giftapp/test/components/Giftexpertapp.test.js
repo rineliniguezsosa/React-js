2fd7f9359cfd9b82b130fd44af4ad486b136078c
rinel <rineliniguezsosa@gmail.com>
2023-02-12 21:29:36 -0600

    importando el componente Giftexpertapp y renderizando el componente

--- giftapp/test/components/Giftexpertapp.test.js
+++ giftapp/test/components/Giftexpertapp.test.js
@@ -1,5 +1,7 @@
 import { render } from "@testing-library/react";
+import { Giftexpertapp } from "../../src/Giftexpertapp";
 
 describe('Pruebas en el componente <Giftexpertapp/>', () => {
+  render(<Giftexpertapp></Giftexpertapp>)
   
 });
