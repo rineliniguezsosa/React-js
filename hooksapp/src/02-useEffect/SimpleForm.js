fc4d9c13021190453c4a3ee6d3216d400f59933e
rinel <rineliniguezsosa@gmail.com>
2023-02-13 13:06:19 -0600

    agregando los valores username y email como valor inicial de los inputs text y email

--- hooksapp/src/02-useEffect/SimpleForm.js
+++ hooksapp/src/02-useEffect/SimpleForm.js
@@ -11,9 +11,9 @@ export const SimpleForm = () => {
     <>
     <span>SimpleForm</span>
     <hr></hr>
-    <input type="text" placeholder="username" name="username" className="form-control"></input>
+    <input type="text" placeholder="username" value={username} name="username" className="form-control"></input>
 
-    <input type="email" placeholder="rineliniguezsosa@gmail.com" name="email" className="form-control mt-2"></input>
+    <input type="email" placeholder="rineliniguezsosa@gmail.com" value={email} name="email" className="form-control mt-2"></input>
     </>
   )
 }
