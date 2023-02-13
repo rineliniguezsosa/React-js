5d91bc8aa2bf3a3e2d858b5fa5d33f2cce604c83
rinel <rineliniguezsosa@gmail.com>
2023-02-12 23:50:48 -0600

    importando el componente Hookapp en el index.js

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Hookapp } from './components/Hookapp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
