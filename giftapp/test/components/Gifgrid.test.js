4901ca99c862ed2fff77e424a7ae8e26263b949a
rinel <rineliniguezsosa@gmail.com>
2023-02-12 18:56:53 -0600

    simulamos la devolucion del custom hook ante un valor inicial cuando no hemos mandado nada del input text

--- giftapp/test/components/Gifgrid.test.js
+++ giftapp/test/components/Gifgrid.test.js
@@ -8,6 +8,12 @@ jest.mock('../../src/hooks/useFetchgifs') //jest.mock simula un modulo en esta c
 describe('Prueba en el componente <Gifgrid/>', () => {
   const usercategory = 'goku'
   test('debe de mostrar el loading inicialmente', () => {
+
+    useFetchgifs.mockReturnValue({ //3 simulamos la devolucion del mock o de la funcion
+      images:[],
+      cargando:true,
+    })
+
     render(<Gifgrid usercategory={usercategory}></Gifgrid>)
     
     expect(screen.getByText('Cargando ...')).toBeTruthy();
