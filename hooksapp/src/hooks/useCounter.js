643c06320dc7bdc0ae002816c3a98d2c48dac127
rinel <rineliniguezsosa@gmail.com>
2023-02-13 11:56:30 -0600

    actualizando el state de el valorinicial sumadole un valor

--- hooksapp/src/hooks/useCounter.js
+++ hooksapp/src/hooks/useCounter.js
@@ -4,7 +4,7 @@ export const useCounter = (valorinicial) => {
   const [value, setValue] = useState(valorinicial) 
   
   const increment = () =>{
-    
+    setValue(value+1)
   }
   return {
     value,
