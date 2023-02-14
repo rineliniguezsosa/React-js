68edf4767a0594c032e147f831f9c115a78edb88
rinel <rineliniguezsosa@gmail.com>
2023-02-14 15:23:49 -0600

    creando un nuevo input text de tipo password

--- hooksapp/src/components/FormCustomHook.js
+++ hooksapp/src/components/FormCustomHook.js
@@ -26,6 +26,8 @@ export const FormCustomHook = () => {
     <input type="text" onChange={inputchange} placeholder="username" value={username} name="username" className="form-control"></input>
 
     <input type="email" onChange={inputchange} placeholder="rineliniguezsosa@gmail.com" value={email} name="email" className="form-control mt-2"></input>
+
+    <input type="password" onChange={inputchange} placeholder="password" value={password} name="password" className="form-control mt-2"></input>
     </>
   )
 }
