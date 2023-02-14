38bf5def076b54cd5de2c7dcabc907f4a74cb155
rinel <rineliniguezsosa@gmail.com>
2023-02-14 15:11:09 -0600

    pegando el contenido de SimpleForm.js al FormCustomHook.js

---
+++ hooksapp/src/components/FormCustomHook.js
@@ -0,0 +1,31 @@
+import { useState,useEffect } from 'react';
+
+export const SimpleForm = () => {
+    const [form, setForm] = useState({
+        username:'rinel',
+        email:'rineliniguezsosa@gmail.com'
+    })
+
+    const {username,email} = form
+    
+    const inputchange = (event) =>{
+        const {name,value} = event.target
+        setForm({
+          ...form,
+          [name]:value //PROPIEDAS COMPUTADAS DE OBJETOS
+      })
+    }
+
+    useEffect(()=>{
+      console.log('Me ejecute')
+    },[email])
+  return (
+    <>
+    <span>formulario con CustomHook</span>
+    <hr></hr>
+    <input type="text" onChange={inputchange} placeholder="username" value={username} name="username" className="form-control"></input>
+
+    <input type="email" onChange={inputchange} placeholder="rineliniguezsosa@gmail.com" value={email} name="email" className="form-control mt-2"></input>
+    </>
+  )
+}
