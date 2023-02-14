fb8b7e4385565a1db13e438260ca09db0e77d521
rinel <rineliniguezsosa@gmail.com>
2023-02-14 15:13:43 -0600

    importanddo el componente FormCustomHook en el index.js y comentando el modo stricto de react

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -4,10 +4,11 @@ import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { Counterapp } from './01-useState/Counterapp';
 //import { CounterCustomHook } from './components/CounterCustomHook';
 //import { Hookapp } from './components/Hookapp';
+import { FormCustomHook } from './components/FormCustomHook'
 import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <SimpleForm></SimpleForm>
-    </React.StrictMode>
+    // </React.StrictMode>
 )
