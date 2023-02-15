ecd7615d6fd766ed401cdcb7c9a38ee19351e7d6
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:11:16 -0600

    consultando la api breaj¿king bad mediante el fetch y transformando los datos en formato json()

--- hooksapp/src/hooks/useFetch.js
+++ hooksapp/src/hooks/useFetch.js
@@ -3,7 +3,9 @@ import { useEffect } from 'react';
 export const useFetch = (url) => {
 
  const getBreakinbadapi = async (url) =>{
-
+    const req = await fetch(url)
+    const data = await req.json()
+    console.log(data)
  }
 
   useEffect(() => {
