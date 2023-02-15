ba0755ffad04d67f6109ece54cd44c5d200db0e3
rinel <rineliniguezsosa@gmail.com>
2023-02-14 18:01:46 -0600

    comentando FormCustomHook

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 //import { Counterapp } from './01-useState/Counterapp';
 //import { CounterCustomHook } from './components/CounterCustomHook';
 //import { Hookapp } from './components/Hookapp';
-import { FormCustomHook } from './components/FormCustomHook'
+// import { FormCustomHook } from './components/FormCustomHook'
 import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
