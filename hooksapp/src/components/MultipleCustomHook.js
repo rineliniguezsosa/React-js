c5734ff87a8e8d58177047797a990feae5cb7459
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:33:56 -0600

    agregando fragment y agregando a un orden al html

--- hooksapp/src/components/MultipleCustomHook.js
+++ hooksapp/src/components/MultipleCustomHook.js
@@ -5,6 +5,8 @@ export const MultipleCustomHook = () => {
   const {datos} = useFetch("https://api.breakingbadquotes.xyz/v1/quotes/5")
   console.log(datos)
   return (
-    <div>MultipleCustomHook</div>
+    <>
+    <h1>MultipleCustomHook</h1>
+    </>
   )
 }
