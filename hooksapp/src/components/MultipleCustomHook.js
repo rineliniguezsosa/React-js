82bb6b094be9e89a50cba03b1ef4148e733410ff
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:52:14 -0600

    desestrcuturando de datos la data y desestructurando de data el author y el quote

--- hooksapp/src/components/MultipleCustomHook.js
+++ hooksapp/src/components/MultipleCustomHook.js
@@ -4,7 +4,9 @@ import { useFetch } from "../hooks/useFetch"
 export const MultipleCustomHook = () => {
   const {datos} = useFetch("https://api.breakingbadquotes.xyz/v1/quotes/5")
   console.log(datos)
-  const {data,cargando,errores} = datos
+  const {data,cargando} = datos
+  const {author,quote} = !!data && data[0]
+  console.log(author,quote);
   return (
     <>
     <h1>MultipleCustomHook</h1>
