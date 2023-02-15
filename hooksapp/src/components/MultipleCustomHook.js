6c6a84a662585d7975e3e0390ea8de08fe92ec84
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:35:07 -0600

    desestructurando datos y agregando un elemento hr

--- hooksapp/src/components/MultipleCustomHook.js
+++ hooksapp/src/components/MultipleCustomHook.js
@@ -4,9 +4,11 @@ import { useFetch } from "../hooks/useFetch"
 export const MultipleCustomHook = () => {
   const {datos} = useFetch("https://api.breakingbadquotes.xyz/v1/quotes/5")
   console.log(datos)
+  const {data,cargando,errores} = datos
   return (
     <>
     <h1>MultipleCustomHook</h1>
+    <hr></hr>
     </>
   )
 }
