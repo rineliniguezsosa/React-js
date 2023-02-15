d5a67d4eb1237744fa9767d84c87517eb4966fa4
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:01:54 -0600

    llamando al useFetch y pasando la url de api a consultar

--- hooksapp/src/components/MultipleCustomHook.js
+++ hooksapp/src/components/MultipleCustomHook.js
@@ -2,6 +2,7 @@ import { useFetch } from "../hooks/useFetch"
 
 
 export const MultipleCustomHook = () => {
+  const {} = useFetch("https://api.breakingbadquotes.xyz/v1/quotes/5")
   return (
     <div>MultipleCustomHook</div>
   )
