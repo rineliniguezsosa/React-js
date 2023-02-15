4b6bb442d58f1ea04c1233183eadba5d8cbdf264
rinel <rineliniguezsosa@gmail.com>
2023-02-15 17:17:25 -0600

    importando useState y agregando un comentario sobre el hook memo

--- hooksapp/src/04-memo/Memorize.js
+++ hooksapp/src/04-memo/Memorize.js
@@ -1,5 +1,6 @@
 import { useCounter } from "../hooks/useCounter"
 import { Small } from "./Small"
+import { useState } from 'react';
 
 export const Memorize = () => {
   const {value,actions} = useCounter(1)
@@ -9,7 +10,14 @@ export const Memorize = () => {
     <h1>Memorize <Small value={value}></Small></h1>
     <hr></hr>
     <button onClick={increment} className="btn btn-primary">+1</button>
-    {/* contexto: por cada cambio en el state react actualiza el dom */}
+    {/* 
+        contexto: por cada cambio en el state react actualiza el dom 
+
+        si en el componente PADRE <Memorize/> sucede una nueva actualizacion 
+        y en el componente hijo <Small/> no tiene nada que ver con el cambio sucedido 
+        entonces por lo cual react no deberia de renderizarse de nueva cuenta el componente 
+        <Small/>
+    */}
     </>
   )
 }
