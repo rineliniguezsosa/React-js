fe257d319d52c42060077e4ad0e6799e37ff4d78
rinel <rineliniguezsosa@gmail.com>
2023-02-15 13:47:30 -0600

    importando FocusScreen y comentando la importacion de MultipleCustomHook

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MultipleCustomHook } from './components/MultipleCustomHook';
+import { FocusScreen } from './03-useRef/FocusScreen';
+//import { MultipleCustomHook } from './components/MultipleCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { Counterapp } from './01-useState/Counterapp';
 //import { CounterCustomHook } from './components/CounterCustomHook';
