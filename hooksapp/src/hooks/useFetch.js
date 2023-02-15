11b49f2a5402ab7b6039039af995f24831eedddc
rinel <rineliniguezsosa@gmail.com>
2023-02-14 20:13:17 -0600

    inicializando el state como un objeto

--- hooksapp/src/hooks/useFetch.js
+++ hooksapp/src/hooks/useFetch.js
@@ -1,7 +1,11 @@
 import { useEffect,useState } from 'react';
 
 export const useFetch = (url) => {
-
+ const [datos, setDatos] = useState({
+    data:null,
+    cargando:true,
+    errores:null
+ })
  const getBreakinbadapi = async (url) =>{
     const req = await fetch(url)
     const data = await req.json()
