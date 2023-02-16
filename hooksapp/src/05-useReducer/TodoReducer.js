924b39a3692845d887da79554492e8fb508f3fe3
rinel <rineliniguezsosa@gmail.com>
2023-02-16 16:52:10 -0600

    agregando provisionalmente el switch case

--- hooksapp/src/05-useReducer/TodoReducer.js
+++ hooksapp/src/05-useReducer/TodoReducer.js
@@ -1,4 +1,11 @@
 
 export const reducer = (inicialState,action) =>{
-
+    switch (action) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
 }
