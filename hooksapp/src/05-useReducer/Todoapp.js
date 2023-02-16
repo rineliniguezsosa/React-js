759aeda982587f98820279d1c12ef4452a4b3eb1
rinel <rineliniguezsosa@gmail.com>
2023-02-16 16:34:08 -0600

    creando el array de objetos initialState

--- hooksapp/src/05-useReducer/Todoapp.js
+++ hooksapp/src/05-useReducer/Todoapp.js
@@ -1,6 +1,13 @@
 import { useReducer } from 'react';
 
 export function Todoapp() {
+  const initialState = [
+    {
+    id:new Date().getDate(),
+    descripcion:'tomar el curso de js',
+    realizado:true
+    }
+] 
   const [state, dispatch] = useReducer(reducer,initialState, third)
   return (
     <>
