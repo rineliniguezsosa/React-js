84c99766831fba5658d4047104e3945cd1a6a9a5
rinel <rineliniguezsosa@gmail.com>
2023-02-16 17:29:56 -0600

    agregamos display-flex d-flex y justify-content-between

--- hooksapp/src/05-useReducer/Todoapp.js
+++ hooksapp/src/05-useReducer/Todoapp.js
@@ -17,7 +17,7 @@ export function Todoapp() {
     <div className='row'>
       <div className='col-7'>
         <ul className="list-group">
-            <li className="list-group-item">
+            <li className="list-group-item d-flex justify-content-between">
               <span className="align-self-center">item</span>
               <button className="btn btn-danger">Eliminar</button>
             </li>
