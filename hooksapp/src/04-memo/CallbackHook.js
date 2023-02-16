998c1f7519641ec0465ccb745bd2ef0839ac2cc2
rinel <rineliniguezsosa@gmail.com>
2023-02-16 11:22:11 -0600

    creando el arrow function incrementar

--- hooksapp/src/04-memo/CallbackHook.js
+++ hooksapp/src/04-memo/CallbackHook.js
@@ -3,6 +3,10 @@ import { Showincrement } from './Showincrement';
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10)
+
+  const incrementar = () =>{
+    
+  }
   return (
     <>
     <span>CallbackHook</span>
