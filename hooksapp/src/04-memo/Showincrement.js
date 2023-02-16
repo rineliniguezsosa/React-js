b9c7220de61c1334013c24287ad6fb216e1b2da0
rinel <rineliniguezsosa@gmail.com>
2023-02-16 11:17:39 -0600

    desestructurando la referencia de la funcion incrementar el cual es desestructurada por props

--- hooksapp/src/04-memo/Showincrement.js
+++ hooksapp/src/04-memo/Showincrement.js
@@ -1,6 +1,6 @@
 
 
-export const Showincrement = () => {
+export const Showincrement = ({incrementar}) => {
   return (
     <button onClick={()=> incrementar()} className="btn btn-primary">increment</button>
   )
