6199a266a5284d7bdc7412d5157782a90bcdd89a
rinel <rineliniguezsosa@gmail.com>
2023-02-16 10:24:08 -0600

    comentando la importacion Memorize y renderizando el componente MemoHook.js

--- hooksapp/src/index.js
+++ hooksapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MemoHook } from './04-memo/MemoHook';
 //import { FocusScreen } from './03-useRef/FocusScreen';
-import { Memorize } from './04-memo/Memorize';
+//import { Memorize } from './04-memo/Memorize';
 //import { MultipleCustomHook } from './components/MultipleCustomHook';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { Counterapp } from './01-useState/Counterapp';
@@ -13,6 +13,6 @@ import './estilos.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-        <Memorize></Memorize>
+        <MemoHook></MemoHook>
     // </React.StrictMode>
 )
