d9d18b0bfdf9ef3860f855598a31caacdd2643a7
rinel <rineliniguezsosa@gmail.com>
2023-02-16 12:45:47 -0600

    asignando el argumento 5  y imprimiendo en consola

--- hooksapp/src/04-memo/Showincrement.js
+++ hooksapp/src/04-memo/Showincrement.js
@@ -1,7 +1,8 @@
 
 
 export const Showincrement = ({incrementar}) => {
+    console.log('me volvi a incrementar')
   return (
-    <button onClick={()=> incrementar()} className="btn btn-primary">increment</button>
+    <button onClick={()=> incrementar(5)} className="btn btn-primary">increment</button>
   )
 }
