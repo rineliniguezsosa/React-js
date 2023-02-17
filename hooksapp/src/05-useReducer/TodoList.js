5bb15402685277ae850477f50893f87556eaf0e6
rinel <rineliniguezsosa@gmail.com>
2023-02-17 12:13:46 -0600

    pagando el contenido del los items a generar

--- hooksapp/src/05-useReducer/TodoList.js
+++ hooksapp/src/05-useReducer/TodoList.js
@@ -2,6 +2,11 @@
 
 export const TodoList = () => {
   return (
-    <div>TodoList</div>
+        <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">item</span>
+              <button className="btn btn-danger">Eliminar</button>
+            </li>
+        </ul>
   )
 }
