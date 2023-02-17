d23ab840d18d1f08a65f3af2ff45a1f6065aad58
rinel <rineliniguezsosa@gmail.com>
2023-02-17 13:13:27 -0600

    desestructurando las props y asignando las props en el jsx

--- hooksapp/src/05-useReducer/TodoList.js
+++ hooksapp/src/05-useReducer/TodoList.js
@@ -1,10 +1,10 @@
 
 
-export const TodoList = () => {
+export const TodoList = ({id,descripcion}) => {
   return (
         <ul className="list-group">
-            <li className="list-group-item d-flex justify-content-between">
-              <span className="align-self-center">item</span>
+            <li key={id} className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">{descripcion}</span>
               <button className="btn btn-danger">Eliminar</button>
             </li>
         </ul>
