94b93d42ddf0168f16321b98684d588d7a972c2e
rinel <rineliniguezsosa@gmail.com>
2023-02-21 11:15:37 -0600

    pegando la funcion handleform y devolviendo la referencia de la funcion

--- hooksapp/src/hooks/useTodo.js
+++ hooksapp/src/hooks/useTodo.js
@@ -1,8 +1,15 @@
 
 export const useTodo = () =>{
 
+    const handleform = (todo) =>{
+        const action = {
+          type:'agregar',
+          payload:todo,
+        }
+        dispatch(action)
+    }
     return {
-        
+        handleform
     }
 
 }
