8feb1565e20c9e6855f680b3129c3b7d8689ae95
rinel <rineliniguezsosa@gmail.com>
2023-02-20 23:21:47 -0600

    agregando la funcion de flecha

--- hooksapp/src/05-useReducer/Todoitem.js
+++ hooksapp/src/05-useReducer/Todoitem.js
@@ -5,7 +5,7 @@ export const Todoitem = ({todo,onremoveItem,completado}) => {
     <li  className="list-group-item d-flex justify-content-between">
               <span className="align-self-center">{todo.descripcion}</span>
               <button className="btn btn-danger" onClick={()=>onremoveItem(todo.id)}>Eliminar</button>
-              <button type="button" onClick={completado(todo.id)} className="btn btn-success">completado</button>
+              <button type="button" onClick={()=>completado(todo.id)} className="btn btn-success">completado</button>
     </li>
   )
 }
