030d922455037cd02f18ee6d6db4b62a1057b4b2
rinel <rineliniguezsosa@gmail.com>
2023-02-21 15:08:03 -0600

    moviendo el contenido de elemento h1 y hr

--- hooksapp/src/06-useContext/Main.js
+++ hooksapp/src/06-useContext/Main.js
@@ -3,9 +3,10 @@ import { Routes } from 'react-router-dom'
 export const Main = () => {
   return (
     <>
-    <Routes>
-      <h1>Main</h1>
+    <h1>Main</h1>
       <hr></hr>
+    <Routes>
+      
     </Routes>
     </>
   )
