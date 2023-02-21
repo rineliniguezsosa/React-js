d3acb18c9cecf254f7d8e4ff6516e5e9c5ad7ef3
rinel <rineliniguezsosa@gmail.com>
2023-02-20 22:32:21 -0600

    filtrando los ids para removerlos

--- hooksapp/src/05-useReducer/TodoReducer.js
+++ hooksapp/src/05-useReducer/TodoReducer.js
@@ -3,7 +3,8 @@ export const reducer = (initialState,action) =>{
     switch (action.type) {
         case 'agregar':
             return [...initialState,action.payload]
-    
+        case 'eliminar':
+            return initialState.filter(removeitem => removeitem.id !== action.payload)
         default:
             break;
     }
