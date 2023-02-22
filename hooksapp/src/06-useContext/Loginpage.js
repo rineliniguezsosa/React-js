bc297a320dcfd3d28e7964d772e08837a17b9ccd
rinel <rineliniguezsosa@gmail.com>
2023-02-21 20:54:02 -0600

    consumiendo el contexto y desestructurando el objeto usuario

--- hooksapp/src/06-useContext/Loginpage.js
+++ hooksapp/src/06-useContext/Loginpage.js
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 import { UserContext } from './Context/UserContext'
 
 export const Loginpage = () => {
+  const { usuario } = useContext(UserContext)
+  console.log(usuario)
   return (
     <div>Loginpage</div>
   )
