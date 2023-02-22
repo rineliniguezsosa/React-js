9d810d86c52a937f0da2b648435a31afc945495c
rinel <rineliniguezsosa@gmail.com>
2023-02-22 15:19:56 -0600

    desestructurando el value de result.currrent y haciendo la prueba esperando a que  el valor sea igual a 10

--- hooksapp/src/test/hooks/useCounter.test.js
+++ hooksapp/src/test/hooks/useCounter.test.js
@@ -5,5 +5,8 @@ import { renderHook } from '@testing-library/react'
 describe('Pruebas en el hook personalizado useCounter', () => {
   test('debe de retonar los valores por defecto', () => {
     const {result} = renderHook(()=> useCounter(10));
+    const {value} = result.current
+    console.log(result)
+    expect(value).toBe(10);
   });
 });
