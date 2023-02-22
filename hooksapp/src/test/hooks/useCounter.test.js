4fc8f2cf9f974261e032087a751548f2c6efc564
rinel <rineliniguezsosa@gmail.com>
2023-02-22 17:02:09 -0600

    eliminamos los skip de las pruebas y añadimos el renderHook para probar el customhook useCounter

--- hooksapp/src/test/hooks/useCounter.test.js
+++ hooksapp/src/test/hooks/useCounter.test.js
@@ -3,14 +3,14 @@ import { renderHook,act } from '@testing-library/react'
 
 
 describe('Pruebas en el hook personalizado useCounter', () => {
-  test.skip('debe de retonar los valores por defecto', () => {
+  test('debe de retonar los valores por defecto', () => {
     const {result} = renderHook(()=> useCounter(10));
     const {value} = result.current
     console.log(result)
     expect(value).toBe(10);
   });
 
-  test.skip('debe de incrementar el contador', () => {
+  test('debe de incrementar el contador', () => {
     const {result} = renderHook(()=> useCounter(20));
     const [incrementar] = result.current.actions
     
@@ -34,6 +34,7 @@ describe('Pruebas en el hook personalizado useCounter', () => {
   });
 
   test('debe de realizar el reset', () => {
+    const {result} = renderHook(()=> useCounter(50));
     
   });
 });
