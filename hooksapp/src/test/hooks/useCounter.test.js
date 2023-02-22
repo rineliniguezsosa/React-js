de5921b54f2ea324f0c71668cad0972652ca21f9
rinel <rineliniguezsosa@gmail.com>
2023-02-22 14:47:42 -0600

    importando useCounter

--- hooksapp/src/test/hooks/useCounter.test.js
+++ hooksapp/src/test/hooks/useCounter.test.js
@@ -1,3 +1,5 @@
+import { useCounter } from "../../hooks/useCounter";
+
 describe('Pruebas en el hook personalizado useCounter', () => {
   test('debe de retonar los valores por defecto', () => {
     
