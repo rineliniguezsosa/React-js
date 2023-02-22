b46fe9a808764fb3fd9293ff04e641f7027b3072
rinel <rineliniguezsosa@gmail.com>
2023-02-22 14:51:02 -0600

    importando renderHook de react-library/react

--- hooksapp/src/test/hooks/useCounter.test.js
+++ hooksapp/src/test/hooks/useCounter.test.js
@@ -1,4 +1,6 @@
 import { useCounter } from "../../hooks/useCounter";
+import { renderHook } from '@testing-library/react'
+
 
 describe('Pruebas en el hook personalizado useCounter', () => {
   test('debe de retonar los valores por defecto', () => {
