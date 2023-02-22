615fa19bcc1b8d6314d0632c9631e82d188fd1c0
rinel <rineliniguezsosa@gmail.com>
2023-02-21 18:42:02 -0600

    pegando los componentes Link en el componente Navbar

--- hooksapp/src/components/Navbar.js
+++ hooksapp/src/components/Navbar.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <>
-    Navbar
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
     </>
   )
 }
