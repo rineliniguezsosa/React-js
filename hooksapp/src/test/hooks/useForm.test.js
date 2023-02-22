50f97bdfa3e6f7c70277b7e352f15e9dd6c7f2f7
rinel <rineliniguezsosa@gmail.com>
2023-02-22 17:24:09 -0600

    importando renderHook para probar customhooks

--- hooksapp/src/test/hooks/useForm.test.js
+++ hooksapp/src/test/hooks/useForm.test.js
@@ -1,3 +1,7 @@
+import { renderHook } from "@testing-library/react";
+
+
+
 describe('pruebas en el customhook useForm', () => {
     test('debe de retornar la informacion por defecto', () => {
         
