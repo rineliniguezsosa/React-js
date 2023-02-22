fc77e85e13483651e71054181f3ebdeacf79a945
rinel <rineliniguezsosa@gmail.com>
2023-02-22 15:16:22 -0600

    simulando el customhook mediante renderHook y desestructurando el result

--- hooksapp/src/test/hooks/useCounter.test.js
+++ hooksapp/src/test/hooks/useCounter.test.js
@@ -4,6 +4,6 @@ import { renderHook } from '@testing-library/react'
 
 describe('Pruebas en el hook personalizado useCounter', () => {
   test('debe de retonar los valores por defecto', () => {
-    
+    const {result} = renderHook(()=> useCounter(10));
   });
 });
