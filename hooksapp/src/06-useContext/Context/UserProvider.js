b4566ba81c83f2bc1e636aef537623b16b148d27
rinel <rineliniguezsosa@gmail.com>
2023-02-21 20:49:34 -0600

    creeando el objeto usuario y pasando al provider

--- hooksapp/src/06-useContext/Context/UserProvider.js
+++ hooksapp/src/06-useContext/Context/UserProvider.js
@@ -2,8 +2,12 @@ import { UserContext } from "./UserContext"
 
 
 export const UserProvider = ({children}) => {
+    const usuario = {
+        id:123,
+        nombre:"rinel iñiguez sosa"
+    }
   return (
-    <UserContext.Provider value={{nombre:'rinel iñiguez'}}>
+    <UserContext.Provider value={{nombre:'rinel iñiguez',usuario}}>
         {children}
     </UserContext.Provider>
   )
