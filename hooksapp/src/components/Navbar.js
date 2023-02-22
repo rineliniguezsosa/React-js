95546b93055f964be5d091004422791bec9aa528
rinel <rineliniguezsosa@gmail.com>
2023-02-21 19:21:09 -0600

    condicionando la ruta activa

--- hooksapp/src/components/Navbar.js
+++ hooksapp/src/components/Navbar.js
@@ -14,10 +14,10 @@ export const Navbar = () => {
                     <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                    <NavLink className="nav-link" to="/about">About</NavLink>
+                    <NavLink className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`} to="/about">About</NavLink>
                     </li>
                     <li className="nav-item">
-                    <NavLink className="nav-link" to="/login">Login</NavLink>
+                    <NavLink className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`} to="/login">Login</NavLink>
                     </li>
                 </ul>
                 </div>
