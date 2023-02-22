5f26c5e8022876f35bea5e04f1cee4ac00308bb1
rinel <rineliniguezsosa@gmail.com>
2023-02-21 18:42:59 -0600

    importando el componente Navbar en el archivo Main.js

--- hooksapp/src/06-useContext/Main.js
+++ hooksapp/src/06-useContext/Main.js
@@ -2,6 +2,7 @@ import { Routes,Route,Navigate,Link } from 'react-router-dom'
 import { Homepage } from '../06-useContext/Homepage'
 import { Aboutpage } from '../06-useContext/Aboutpage'
 import { Loginpage } from '../06-useContext/Loginpage'
+import { Navbar } from '../components/Navbar'
 
 
 export const Main = () => {
@@ -9,9 +10,9 @@ export const Main = () => {
     <>
     <h1>Main</h1>
       <hr></hr>
-      <Link to="/">Home</Link>
+      {/* <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
+      <Link to="/login">Login</Link> */}
     <Routes>
       <Route path="/" element={<Homepage></Homepage>}></Route>
       <Route path="/about" element={<Aboutpage></Aboutpage>}></Route>
