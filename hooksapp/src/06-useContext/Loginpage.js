09878029d999b2f063e0469eaf609a9451eee58d
rinel <rineliniguezsosa@gmail.com>
2023-02-21 22:18:10 -0600

    desestructurando la funcion setUser,actualizando el state donde obtendremos los valores anteriores y agregamos un nuevo objeto

--- hooksapp/src/06-useContext/Loginpage.js
+++ hooksapp/src/06-useContext/Loginpage.js
@@ -2,15 +2,16 @@ import { useContext } from 'react'
 import { UserContext } from './Context/UserContext'
 
 export const Loginpage = () => {
-  const { usuario } = useContext(UserContext)
-  console.log(usuario)
+  const { user,setUser } = useContext(UserContext)
+  console.log(user)
   return (
     <>
     <h1>Loginpage</h1>
     <hr></hr>
-    {JSON.stringify(usuario)}
+    {JSON.stringify(user)}
 
-    <button className="btn btn-primary">Agregar</button>
+    <button onClick={()=> setUser([...user,{id:124,
+        nombre:"juan pablo"}])} className="btn btn-primary">Agregar</button>
     </>
   )
 }
