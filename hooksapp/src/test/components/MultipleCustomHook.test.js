cf64a340a4de54b0a6966bd7104b14c044cb740a
rinel <rineliniguezsosa@gmail.com>
2023-02-23 14:50:04 -0600

    importando el customhook useFetch

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -1,5 +1,6 @@
 import {render,screen} from '@testing-library/react'
 import { MultipleCustomHook } from '../../components/MultipleCustomHook';
+import { useFetch } from '../../hooks/useFetch';
 
 describe('Pruebas en MultipleCustomHook', () => {
   test('debe de mostrar el componente', () => {
