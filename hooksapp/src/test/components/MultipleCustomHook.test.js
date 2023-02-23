0a9e78d2753dc34e6be8799a35c2761537545cac
rinel <rineliniguezsosa@gmail.com>
2023-02-23 12:17:18 -0600

    importando screen de testing-libray/react

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -1,4 +1,4 @@
-import {render} from '@testing-library/react'
+import {render,screen} from '@testing-library/react'
 import { MultipleCustomHook } from '../../components/MultipleCustomHook';
 
 describe('Pruebas en MultipleCustomHook', () => {
