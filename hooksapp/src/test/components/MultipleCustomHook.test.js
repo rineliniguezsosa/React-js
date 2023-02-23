891ed895224909ace4b3a7d54cb342a4e64ade5a
rinel <rineliniguezsosa@gmail.com>
2023-02-23 12:09:25 -0600

    importando el componente MultipleCustomHook

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -1,7 +1,8 @@
 import {render} from '@testing-library/react'
+import { MultipleCustomHook } from '../../components/MultipleCustomHook';
 
 describe('Pruebas en MultipleCustomHook', () => {
   test('debe de mostrar el componente', () => {
-    
+  
   });
 });
