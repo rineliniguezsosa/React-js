43d0290f3c3c4d68cbca70df706d011791a582c1
rinel <rineliniguezsosa@gmail.com>
2023-02-23 14:36:05 -0600

    renderizando el componente MultipleCustomHook

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -3,6 +3,6 @@ import { MultipleCustomHook } from '../../components/MultipleCustomHook';
 
 describe('Pruebas en MultipleCustomHook', () => {
   test('debe de mostrar el componente', () => {
-  
+    render(<MultipleCustomHook></MultipleCustomHook>)
   });
 });
