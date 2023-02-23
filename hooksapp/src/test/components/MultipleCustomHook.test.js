760607af91f1f976553d9e7d4204ad70860b8de1
rinel <rineliniguezsosa@gmail.com>
2023-02-23 15:54:39 -0600

    importando fireEvent para el evento de click

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -1,4 +1,4 @@
-import {render,screen} from '@testing-library/react'
+import {render,screen,fireEvent} from '@testing-library/react'
 import { MultipleCustomHook } from '../../components/MultipleCustomHook';
 import { useFetch } from '../../hooks/useFetch';
 
