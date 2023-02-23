32083bda299a911e33e3deaa7d94120a8711ce1a
rinel <rineliniguezsosa@gmail.com>
2023-02-23 14:38:46 -0600

    obteniendo el boton mediante el getByRole y añadiendo mas opciones de busqueda mediante el name Next quote

--- hooksapp/src/test/components/MultipleCustomHook.test.js
+++ hooksapp/src/test/components/MultipleCustomHook.test.js
@@ -4,5 +4,8 @@ import { MultipleCustomHook } from '../../components/MultipleCustomHook';
 describe('Pruebas en MultipleCustomHook', () => {
   test('debe de mostrar el componente', () => {
     render(<MultipleCustomHook></MultipleCustomHook>)
+
+    const elemento = screen.getByRole('button',{name:'Next quote'})
+    
   });
 });
