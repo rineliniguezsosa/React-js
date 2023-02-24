84154df955df594d1e93b2a57860e8a3e166e615
rinel <rineliniguezsosa@gmail.com>
2023-02-24 13:55:26 -0600

    simulamos las funciones onremoveItem y completado mediante jest.fn

--- hooksapp/src/test/05-useReducer/Todoitem.test.js
+++ hooksapp/src/test/05-useReducer/Todoitem.test.js
@@ -6,6 +6,9 @@ describe('pruebas en el componente <Todoitem/>', () => {
         realizado:false
     }
 
+    const onremoveItem = jest.fn()
+    const completado = jest.fn()
+
     test('debe de mostrar pendiente por completar', () => {
         
     });
