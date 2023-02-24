0efa6c6645457119e5b2e0e409167909f7013f4f
rinel <rineliniguezsosa@gmail.com>
2023-02-24 13:49:56 -0600

    creando el objeto todo con las props id,descripcion y realizado

--- hooksapp/src/test/05-useReducer/Todoitem.test.js
+++ hooksapp/src/test/05-useReducer/Todoitem.test.js
@@ -1,5 +1,11 @@
 describe('pruebas en el componente <Todoitem/>', () => {
-    
+
+    const todo = {
+        id:1,
+        descripcion:'curso react router',
+        realizado:false
+    }
+
     test('debe de mostrar pendiente por completar', () => {
         
     });
