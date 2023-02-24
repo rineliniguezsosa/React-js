12b6815fbe18847b04bff04903785b208496c978
rinel <rineliniguezsosa@gmail.com>
2023-02-24 16:25:01 -0600

    correccion en la ruta del Todoapp

--- hooksapp/src/test/05-useReducer/Todoapp.test.js
+++ hooksapp/src/test/05-useReducer/Todoapp.test.js
@@ -1,5 +1,5 @@
 import { render,screen } from '@testing-library/react'
-import { Todoapp } from '../../05-useReducer/Todoadd';
+import { Todoapp } from '../../05-useReducer/Todoapp';
 
 
 describe('pruebas en el componente Todoapp', () => {
