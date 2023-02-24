0bf225b7b98d16149a59b4ea2928f6bfb7802e42
rinel <rineliniguezsosa@gmail.com>
2023-02-24 16:20:40 -0600

    importando el componente Todoapp

--- hooksapp/src/test/05-useReducer/Todoapp.test.js
+++ hooksapp/src/test/05-useReducer/Todoapp.test.js
@@ -1,8 +1,10 @@
 import { render } from '@testing-library/react'
+import { Todoapp } from '../../05-useReducer/Todoadd';
+
 
 describe('pruebas en el componente Todoapp', () => {
   
   test('debe de mostrar el componente correctamente', () => {
-    
+
   });
 });
