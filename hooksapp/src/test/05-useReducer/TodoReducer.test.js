bde04173093e9a0bad484a9eb74741bd2c55114d
rinel <rineliniguezsosa@gmail.com>
2023-02-23 20:26:11 -0600

    inicializando el state inicial

--- hooksapp/src/test/05-useReducer/TodoReducer.test.js
+++ hooksapp/src/test/05-useReducer/TodoReducer.test.js
@@ -1,4 +1,11 @@
 describe('pruebas en TodoReducer', () => {
+
+    const initialState = [{
+        id:1,
+        todo:'terminar curso react fh',
+        done:false
+    }]
+
     test('debe de retornar el state inicial', () => {
         
     });
