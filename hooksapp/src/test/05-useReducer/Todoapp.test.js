c818c4377858479886419d0d67a3d7aeb5148d10
rinel <rineliniguezsosa@gmail.com>
2023-02-24 16:29:40 -0600

    importando el customhook useTodo

--- hooksapp/src/test/05-useReducer/Todoapp.test.js
+++ hooksapp/src/test/05-useReducer/Todoapp.test.js
@@ -1,10 +1,12 @@
 import { render,screen } from '@testing-library/react'
 import { Todoapp } from '../../05-useReducer/Todoapp';
+import { useTodo } from '../../hooks/useTodo';
 
 
 describe('pruebas en el componente Todoapp', () => {
   
   test('debe de mostrar el componente correctamente', () => {
     render(<Todoapp/>)
+    screen.debug()
   });
 });
