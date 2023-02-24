bc817f7735e5e31f3597bcba935555ab69408f2a
rinel <rineliniguezsosa@gmail.com>
2023-02-24 15:01:34 -0600

    agregamos el aria-label con el valor de span este aria-label servira de busqueda para el getBylabelText

--- hooksapp/src/05-useReducer/Todoitem.js
+++ hooksapp/src/05-useReducer/Todoitem.js
@@ -3,7 +3,7 @@
 export const Todoitem = ({todo,onremoveItem,completado}) => {
   return (
     <li  className="list-group-item d-flex justify-content-between">
-              <span className={`align-self-center ${todo.realizado ? 'text-decoration-line-through' : ''}`}>{todo.descripcion}</span>
+              <span aria-label="span" className={`align-self-center ${todo.realizado ? 'text-decoration-line-through' : ''}`}>{todo.descripcion}</span>
               <button className="btn btn-danger" onClick={()=>onremoveItem(todo.id)}>Eliminar</button>
               <button type="button" onClick={()=>completado(todo.id)} className="btn btn-success">completado</button>
     </li>
