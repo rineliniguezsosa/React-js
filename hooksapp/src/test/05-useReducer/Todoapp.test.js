ab6292bfec0d78de76bd337dfd3fe4a6714bbb80
rinel <rineliniguezsosa@gmail.com>
2023-02-24 17:14:53 -0600

    retornamos el objeto de useTodo

--- hooksapp/src/test/05-useReducer/Todoapp.test.js
+++ hooksapp/src/test/05-useReducer/Todoapp.test.js
@@ -5,7 +5,14 @@ import { useTodo } from '../../hooks/useTodo';
 jest.mock('../../hooks/useTodo')
 
 describe('pruebas en el componente Todoapp', () => {
-  
+
+  useTodo.mockReturnValue({
+    state:[{id:1,descripcion:'tomar el curso react',realizado:false}],
+    handleform:jest.fn(),
+    removeItem:jest.fn(),
+    tareacompletada:jest.fn()
+  })
+
   test('debe de mostrar el componente correctamente', () => {
     render(<Todoapp/>)
     screen.debug()
