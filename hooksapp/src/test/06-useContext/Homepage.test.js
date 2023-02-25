9718f3ae7286dbc5e35dc3bf159418aee99d92fc
rinel <rineliniguezsosa@gmail.com>
2023-02-24 21:57:37 -0600

    pasamos un objeto con la propiedad nombre como nula y hacemos el screen.debug()

--- hooksapp/src/test/06-useContext/Homepage.test.js
+++ hooksapp/src/test/06-useContext/Homepage.test.js
@@ -1,11 +1,12 @@
 import { UserContext } from "../../06-useContext/Context/UserContext";
 import { Homepage } from "../../06-useContext/Homepage";
-import {render} from '@testing-library/react'
+import {render,screen} from '@testing-library/react'
 
 
 
 describe('pruebas en el componente Homepage', () => {
     test('debe de mostrar el componente sin el usuario', () => {
-        render(<UserContext.Provider><Homepage/></UserContext.Provider>)
+        render(<UserContext.Provider value={{nombre:null}}><Homepage/></UserContext.Provider>)
+        screen.debug()
     });
 });
