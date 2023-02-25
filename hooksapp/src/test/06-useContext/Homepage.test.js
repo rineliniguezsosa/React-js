f86f740b0d9682f5996959b1ae14bce8bedead45
rinel <rineliniguezsosa@gmail.com>
2023-02-24 22:07:38 -0600

    usamos el screen.getByLabeltext para buscar la etiqueta cuyo valor coincida con el nombre y esperamos que el contenido de la etiqueta sea igual a null

--- hooksapp/src/test/06-useContext/Homepage.test.js
+++ hooksapp/src/test/06-useContext/Homepage.test.js
@@ -7,6 +7,11 @@ import {render,screen} from '@testing-library/react'
 describe('pruebas en el componente Homepage', () => {
     test('debe de mostrar el componente sin el usuario', () => {
         render(<UserContext.Provider value={{nombre:null}}><Homepage/></UserContext.Provider>)
+
+        const pre = screen.getByLabelText('nombre')
+
+        expect(pre.innerHTML).toBe('null');
+
         screen.debug()
     });
 });
