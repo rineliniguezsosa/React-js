b4d9a6a847e4c8513258ddd0b0b83982b1901359
rinel <rineliniguezsosa@gmail.com>
2023-02-24 21:15:59 -0600

    crendo el bloque test con la sig descripcion: debe de mostrar el componente sin el usuario

--- hooksapp/src/test/06-useContext/Homepage.test.js
+++ hooksapp/src/test/06-useContext/Homepage.test.js
@@ -3,5 +3,7 @@ import { Homepage } from "../../06-useContext/Homepage";
 
 
 describe('pruebas en el componente Homepage', () => {
-    
+    test('debe de mostrar el componente sin el usuario', () => {
+        
+    });
 });
