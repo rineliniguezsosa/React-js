376c52b23e7b0caf3dd1703a0c332647e35175ca
rinel <rineliniguezsosa@gmail.com>
2023-02-24 23:07:00 -0600

    obteniendo la referencia de la etiqueta pre mediante el nombre

--- hooksapp/src/test/06-useContext/Loginpage.test.js
+++ hooksapp/src/test/06-useContext/Loginpage.test.js
@@ -6,6 +6,7 @@ import {render,screen} from '@testing-library/react'
 describe('pruebas en el componente Loginpage', () => {
     test('debe de mostrar el componente sin el usuario', () => {
         render(<UserContext.Provider value={{user:null}}><Loginpage></Loginpage></UserContext.Provider>)
+        const pre = screen.getByLabelText('nombre')
         
     });
 });
