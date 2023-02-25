a24102553ce894b485c6a85707cd0af459aa818a
rinel <rineliniguezsosa@gmail.com>
2023-02-24 22:55:27 -0600

    importando el UserContext

--- hooksapp/src/test/06-useContext/Loginpage.test.js
+++ hooksapp/src/test/06-useContext/Loginpage.test.js
@@ -1,4 +1,6 @@
+import { UserContext } from '../../06-useContext/Context/UserContext';
 import { Loginpage } from '../../06-useContext/Loginpage'
+
 
 describe('pruebas en el componente Loginpage', () => {
     
