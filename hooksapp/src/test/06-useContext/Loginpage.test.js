9d6f21e507abc56c8e2135b512a9542e03e6a622
rinel <rineliniguezsosa@gmail.com>
2023-02-24 23:08:22 -0600

    obteniendo el contenido de la eqtiqueta pre y esperando a que sea null

--- hooksapp/src/test/06-useContext/Loginpage.test.js
+++ hooksapp/src/test/06-useContext/Loginpage.test.js
@@ -7,6 +7,7 @@ describe('pruebas en el componente Loginpage', () => {
     test('debe de mostrar el componente sin el usuario', () => {
         render(<UserContext.Provider value={{user:null}}><Loginpage></Loginpage></UserContext.Provider>)
         const pre = screen.getByLabelText('nombre')
-        
+        expect(pre.innerHTML).toBe('null');
+        screen.debug()
     });
 });
