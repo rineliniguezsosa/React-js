1705bc7c3b1f5affd7495df843f137c4dd868b74
rinel <rineliniguezsosa@gmail.com>
2023-02-25 00:00:31 -0600

    envolviendo MemoryRouter al componente Main por defecto renderizara la ruta principal Homepage

--- hooksapp/src/test/06-useContext/Main.test.js
+++ hooksapp/src/test/06-useContext/Main.test.js
@@ -6,6 +6,10 @@ describe('pruebas en Main.js react router', () => {
     // <MemoryRouter> almacena sus ubicaciones internamente en una matriz
     // <MemoryRouter initialEntries>el valor predeterminado es ["/"](una sola entrada en la /URL raíz)
     test('debe de renderizar la ruta por defecto', () => {
-      
+      render(
+        <MemoryRouter>
+            <Main></Main>
+        </MemoryRouter>
+      )
     });
 });
