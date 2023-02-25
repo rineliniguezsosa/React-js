7d07b2a6373adeaaeacbffe404b579d865386575
rinel <rineliniguezsosa@gmail.com>
2023-02-24 23:58:59 -0600

    importando MemoryRouter de react-router-dom

--- hooksapp/src/test/06-useContext/Main.test.js
+++ hooksapp/src/test/06-useContext/Main.test.js
@@ -1,10 +1,11 @@
 import { Main } from "../../06-useContext/Main";
 import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('pruebas en Main.js react router', () => {
     // <MemoryRouter> almacena sus ubicaciones internamente en una matriz
     // <MemoryRouter initialEntries>el valor predeterminado es ["/"](una sola entrada en la /URL raíz)
     test('debe de renderizar la ruta por defecto', () => {
-        
+      
     });
 });
