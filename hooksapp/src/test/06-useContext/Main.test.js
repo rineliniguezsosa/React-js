20d872171a05f809978329d8a6d983caf43fde8b
rinel <rineliniguezsosa@gmail.com>
2023-02-24 23:46:27 -0600

    creando el bloque test con la sig descripcion: debe de renderizar la ruta por defecto

--- hooksapp/src/test/06-useContext/Main.test.js
+++ hooksapp/src/test/06-useContext/Main.test.js
@@ -2,5 +2,7 @@ import { Main } from "../../06-useContext/Main";
 
 
 describe('pruebas en Main.js react router', () => {
-    
+    test('debe de renderizar la ruta por defecto', () => {
+        
+    });
 });
