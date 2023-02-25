218530cf4b0fc3419e0e72dcc5e5e4a188640832
rinel <rineliniguezsosa@gmail.com>
2023-02-24 23:49:53 -0600

    comentarios MemoryRouter

--- hooksapp/src/test/06-useContext/Main.test.js
+++ hooksapp/src/test/06-useContext/Main.test.js
@@ -2,6 +2,8 @@ import { Main } from "../../06-useContext/Main";
 
 
 describe('pruebas en Main.js react router', () => {
+    // <MemoryRouter> almacena sus ubicaciones internamente en una matriz
+    // <MemoryRouter initialEntries>el valor predeterminado es ["/"](una sola entrada en la /URL raíz)
     test('debe de renderizar la ruta por defecto', () => {
         
     });
