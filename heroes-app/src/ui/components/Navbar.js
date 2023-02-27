dd2a9a1bd61889b962abe20c1302b72956870cbc
rinel <rineliniguezsosa@gmail.com>
2023-02-27 13:38:12 -0600

    agregando la clase d-flex y justify-content-end

--- heroes-app/src/ui/components/Navbar.js
+++ heroes-app/src/ui/components/Navbar.js
@@ -31,7 +31,7 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">Rinel</span>
                     <NavLink 
