3138ca05addf0598f92bc50b76249fe610d57293
rinel <rineliniguezsosa@gmail.com>
2023-02-27 12:13:12 -0600

    importando el componente AppRouter

--- heroes-app/router/AppRouter.js
+++ heroes-app/router/AppRouter.js
@@ -1,6 +1,7 @@
 import { Routes,Route } from "react-router-dom"
 import { Loginpage } from "../auth/pages/Loginpage"
 import { DCpages } from "../heroes/pages/DCpages"
+import { Marvelpages } from "../heroes/pages/Marvelpages"
 
 
 export const AppRouter = () => {
