3cb93818b83066119afcec188cf0dbdf98fcc9d0
rinel <rineliniguezsosa@gmail.com>
2023-02-27 15:34:49 -0600

    importando el componente Search y creando la ruta search

--- heroes-app/src/heroes/routes/HeroesRoutes.js
+++ heroes-app/src/heroes/routes/HeroesRoutes.js
@@ -1,6 +1,6 @@
 import { Navbar } from "../../ui"
 import {Routes,Route,Navigate} from 'react-router-dom'
-import { Marvelpages,DCpages } from "../pages"
+import { Marvelpages,DCpages,Search } from "../pages"
 
 
 export const HeroesRoutes = () => {
@@ -12,6 +12,7 @@ export const HeroesRoutes = () => {
             <Routes>
                 <Route path="marvel" element={<Marvelpages></Marvelpages>}></Route>
                 <Route path="dc" element={<DCpages></DCpages>}></Route>
+                <Route path="search" element={<Search></Search>}></Route>
                 <Route path="/" element={<Navigate to="/marvel"></Navigate>}></Route>
             </Routes>
         </div>
