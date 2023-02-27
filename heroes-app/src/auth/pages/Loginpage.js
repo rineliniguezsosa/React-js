5a53aa8986886ff6883896e85c3ac72b385df1a2
rinel <rineliniguezsosa@gmail.com>
2023-02-27 16:48:59 -0600

    agregando el elemento h1,button con la clase btn btn-primary

--- heroes-app/src/auth/pages/Loginpage.js
+++ heroes-app/src/auth/pages/Loginpage.js
@@ -3,7 +3,11 @@
 export const Loginpage = () => {
   return (
     <div className="container mt-5">
-
+      <h1>Login</h1>
+      <hr></hr>
+      <button className="btn btn-primary">
+        Login
+      </button>
     </div>
   )
 }
