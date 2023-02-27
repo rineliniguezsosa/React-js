c9d7e7998ec30be7f274613564072ba5c427c205
rinel <rineliniguezsosa@gmail.com>
2023-02-27 17:11:01 -0600

    validando que el publisher no coincida con el publisher obtenido

--- heroes-app/src/heroes/helpers/getHerobyPublisher.js
+++ heroes-app/src/heroes/helpers/getHerobyPublisher.js
@@ -2,4 +2,8 @@ import { heroes } from "../data/heroes";
 
 export const getHeroByPublisher = (publisher) =>{
     const mypublishers = ['DC Comics','Marvel Comics']
+
+    if(!mypublishers.includes(publisher)){
+        throw new Error(`No es un ${publisher} valido,tal vez quisiste escribir DC Comics ó Marvel Comics`)
+    }
 }
