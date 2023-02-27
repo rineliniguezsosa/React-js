18b46aed91513510229785d5801e0094b00fbd2a
rinel <rineliniguezsosa@gmail.com>
2023-02-27 15:09:04 -0600

    importando el component Marvelpages,definiendo las Routes y l aruta de marvel

--- heroes-app/src/heroes/routes/HeroesRoutes.js
+++ heroes-app/src/heroes/routes/HeroesRoutes.js
@@ -1,11 +1,16 @@
 import { Navbar } from "../../ui"
 import {Routes,Route} from 'react-router-dom'
+import { Marvelpages } from "../pages"
 
 
 export const HeroesRoutes = () => {
   return (
     <>
         <Navbar></Navbar>
+
+        <Routes>
+            <Route path="marvel" element={<Marvelpages></Marvelpages>}></Route>
+        </Routes>
     </>
   )
 }
