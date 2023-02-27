ee1510db84130e6ca4eb463862ef8472161a7182
rinel <rineliniguezsosa@gmail.com>
2023-02-27 16:34:15 -0600

    creando el boton y añadiendo la clase btn,añadiendo la referencia de la funcion Logout

--- heroes-app/src/ui/components/Navbar.js
+++ heroes-app/src/ui/components/Navbar.js
@@ -46,12 +46,12 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">Rinel</span>
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/login"
+                    <button 
+                        className="nav-item nav-link btn" 
+                        onClick={Logout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
