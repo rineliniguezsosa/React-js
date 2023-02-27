4fe023a8bd556b567dbdf8728944469014b45430
rinel <rineliniguezsosa@gmail.com>
2023-02-27 16:51:58 -0600

    agregando el evento onClick y pasando por referencia la funcion onLogin donde navegaremos hacia la ruta principal

--- heroes-app/src/auth/pages/Loginpage.js
+++ heroes-app/src/auth/pages/Loginpage.js
@@ -1,11 +1,17 @@
 import { useNavigate } from 'react-router-dom'
 
 export const Loginpage = () => {
+  const navigate = useNavigate()
+  const onLogin = () =>{
+    navigate('/',{
+      replace:true
+    })
+  }
   return (
     <div className="container mt-5">
       <h1>Login</h1>
       <hr></hr>
-      <button className="btn btn-primary">
+      <button className="btn btn-primary" onClick={onLogin}>
         Login
       </button>
     </div>
