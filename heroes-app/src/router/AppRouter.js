6cbab195e56f068ac87033cf7dd6cf943679a622
rinel <rineliniguezsosa@gmail.com>
2023-02-27 13:12:27 -0600

    agregando fragment y añadiendo el componente Navbar al AppRouter

--- heroes-app/src/router/AppRouter.js
+++ heroes-app/src/router/AppRouter.js
@@ -6,11 +6,14 @@ import { Navbar } from "../ui"
 
 export const AppRouter = () => {
   return (
+    <>
+    <Navbar></Navbar>
     <Routes>
         <Route path="marvel" element={<Marvelpages></Marvelpages>}></Route>
         <Route path="dc" element={<DCpages></DCpages>}></Route>
         <Route path="login" element={<Loginpage></Loginpage>}></Route>
         <Route path="/" element={<Navigate to="marvel"></Navigate>}></Route>
     </Routes>
+    </>
   )
 }
