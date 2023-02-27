a31363da6473cc079e87b565b9a7fd4dc3baa247
rinel <rineliniguezsosa@gmail.com>
2023-02-27 15:56:09 -0600

    eliminando la importacion del Navbar y corriendo le importacion  de HeroesRoutes

--- heroes-app/src/router/AppRouter.js
+++ heroes-app/src/router/AppRouter.js
@@ -1,12 +1,10 @@
 import { Routes,Route } from "react-router-dom"
 import { Loginpage } from "../auth"
-import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
-import { Navbar } from "../ui"
+import { HeroesRoutes } from "../heroes"
 
 export const AppRouter = () => {
   return (
     <>
-    <Navbar></Navbar>
     <Routes>
         <Route path="login" element={<Loginpage></Loginpage>}></Route>
     
