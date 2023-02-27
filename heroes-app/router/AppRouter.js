6f470cc30b329df9429ef1bbc27f9294f83d521c
rinel <rineliniguezsosa@gmail.com>
2023-02-27 12:18:00 -0600

    importando Navigate de react-router-dom

--- heroes-app/router/AppRouter.js
+++ heroes-app/router/AppRouter.js
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom"
+import { Routes,Route,Navigate } from "react-router-dom"
 import { Loginpage } from "../auth/pages/Loginpage"
 import { DCpages } from "../heroes/pages/DCpages"
 import { Marvelpages } from "../heroes/pages/Marvelpages"
