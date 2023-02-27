1eb443a4cded7e19db90a22d89df8797c8c42862
rinel <rineliniguezsosa@gmail.com>
2023-02-27 15:23:36 -0600

    envolviendo las rutas por el elemento div con la clase container

--- heroes-app/src/heroes/routes/HeroesRoutes.js
+++ heroes-app/src/heroes/routes/HeroesRoutes.js
@@ -7,12 +7,14 @@ export const HeroesRoutes = () => {
   return (
     <>
         <Navbar></Navbar>
-
-        <Routes>
-            <Route path="marvel" element={<Marvelpages></Marvelpages>}></Route>
-            <Route path="dc" element={<DCpages></DCpages>}></Route>
-            <Route path="/" element={<Navigate to="/marvel"></Navigate>}></Route>
-        </Routes>
+        
+        <div className="container">
+            <Routes>
+                <Route path="marvel" element={<Marvelpages></Marvelpages>}></Route>
+                <Route path="dc" element={<DCpages></DCpages>}></Route>
+                <Route path="/" element={<Navigate to="/marvel"></Navigate>}></Route>
+            </Routes>
+        </div>
     </>
   )
 }
