cb577a269265914393d53477c241852f1a41be6f
rinel <rineliniguezsosa@gmail.com>
2023-02-28 13:16:54 -0600

    creando div con la clase col-4

--- heroes-app/src/heroes/components/Herocard.js
+++ heroes-app/src/heroes/components/Herocard.js
@@ -5,7 +5,9 @@ export const Herocard = ({superhero,publisher,alter,first,characters}) => {
     <div className="col">
         <div className="card">
             <div className="row no-gutters">
+                <div className="col-4">
 
+                </div>
             </div>
         </div>
     </div>
