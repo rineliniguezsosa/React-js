5e533a94cd7c7cdd6396fc3b6392724c71c8bd02
rinel <rineliniguezsosa@gmail.com>
2023-02-28 13:16:07 -0600

    creando el div con la clase row no-gutters

--- heroes-app/src/heroes/components/Herocard.js
+++ heroes-app/src/heroes/components/Herocard.js
@@ -4,7 +4,9 @@ export const Herocard = ({superhero,publisher,alter,first,characters}) => {
   return (
     <div className="col">
         <div className="card">
+            <div className="row no-gutters">
 
+            </div>
         </div>
     </div>
   )
