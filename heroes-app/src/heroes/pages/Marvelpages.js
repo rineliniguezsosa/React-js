fe3cf253196f60790a41a9a3c5355798014649df
rinel <rineliniguezsosa@gmail.com>
2023-02-28 12:41:09 -0600

    creando los elementos fragment,hr,renderizando el componente HeroList y pasando la prop publisher igual a Marvel Comics

--- heroes-app/src/heroes/pages/Marvelpages.js
+++ heroes-app/src/heroes/pages/Marvelpages.js
@@ -3,6 +3,10 @@ import { HeroList } from "../components/HeroList"
 
 export const Marvelpages = () => {
   return (
-    <div>Marvelpages</div>
+    <>
+    <h1>Marvel comics</h1>
+    <hr></hr>
+    <HeroList publisher="Marvel Comics"></HeroList>
+    </>
   )
 }
