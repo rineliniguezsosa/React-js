d6b6c5b7cec9f094ff2d96e29ce909d3b18010b0
rinel <rineliniguezsosa@gmail.com>
2023-02-28 13:14:57 -0600

    creando el div con la clase card

--- heroes-app/src/heroes/components/Herocard.js
+++ heroes-app/src/heroes/components/Herocard.js
@@ -3,7 +3,9 @@
 export const Herocard = ({superhero,publisher,alter,first,characters}) => {
   return (
     <div className="col">
-       
+        <div className="card">
+
+        </div>
     </div>
   )
 }
