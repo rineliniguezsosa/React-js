27360ffa7faf22ddf579f831e5e3b0b7e8b70897
rinel <rineliniguezsosa@gmail.com>
2023-03-01 14:55:47 -0600

    importando la funcion reducer

--- heroes-app/src/auth/context/AuthProvider.js
+++ heroes-app/src/auth/context/AuthProvider.js
@@ -1,5 +1,6 @@
 import { useContext, useReducer } from "react";
 import { AuthContext } from "./AuthContext";
+import { reducer } from "./AuthReducer";
 
 export const AuthProvider = ({children}) => {
   return (
