e32f7c006a60ffcea2dd6296c8aa8d7fd791a916
rinel <rineliniguezsosa@gmail.com>
2023-03-01 14:59:03 -0600

    definiendo el useReducer y pasandole el estado inical y la funcion reducer

--- heroes-app/src/auth/context/AuthProvider.js
+++ heroes-app/src/auth/context/AuthProvider.js
@@ -3,6 +3,10 @@ import { AuthContext } from "./AuthContext";
 import { reducer } from "./AuthReducer";
 
 export const AuthProvider = ({children}) => {
+    const initialState = {
+        logged:false
+    }
+    const [state, dispatch] = useReducer(initialState, reducer)
   return (
     <AuthContext.Provider>
         {children}
