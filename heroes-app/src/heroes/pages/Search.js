07402b5ce5e0f8b13fcb29e42ccd06ca23ab18d6
rinel <rineliniguezsosa@gmail.com>
2023-02-28 23:41:31 -0600

    importando el useLocation de react-router

--- heroes-app/src/heroes/pages/Search.js
+++ heroes-app/src/heroes/pages/Search.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate,useLocation } from "react-router-dom"
 import { useForm } from "../hooks/useForm"
 
 export const Search = () => {
