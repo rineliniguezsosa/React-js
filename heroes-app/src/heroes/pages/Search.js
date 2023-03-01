ccc348eb420131f385ba65925888438d22032e22
rinel <rineliniguezsosa@gmail.com>
2023-02-28 21:57:57 -0600

    creando multiples elementos h4,hr,form,input con las props type,placeholder,autoComplete y className

--- heroes-app/src/heroes/pages/Search.js
+++ heroes-app/src/heroes/pages/Search.js
@@ -6,7 +6,18 @@ export const Search = () => {
     <span>Search</span>
     <hr></hr>
     <div className="col-5">
-
+      <h4>Busqueda.</h4>
+      <hr></hr>
+      <form>
+        <input 
+        type="text" 
+        placeholder="busqueda" 
+        name="search"
+        autoComplete="off"
+        className="form-control"
+        >
+        </input>
+      </form>
     </div>
 
     <div className="col-7">
