6954c36cdf9a74fde8ed6afcdfe58a146234dc1f
rinel <rineliniguezsosa@gmail.com>
2023-03-01 15:00:49 -0600

    creando bloque switch case donde evaluaremos el action.type sean iguales a el type.login o type.logout

--- heroes-app/src/auth/context/AuthReducer.js
+++ heroes-app/src/auth/context/AuthReducer.js
@@ -2,5 +2,18 @@ import { type } from "../types/types"
 
 
 export const reducer = (state = {},action) =>{
-
+    switch (action.type) {
+        case type.login:
+            return {
+                ...state,
+                logged:true,
+                name:action.payload
+            }
+        case type.logout:
+            return {
+                logged:false
+            }
+        default:
+            return state;
+    }
 }
