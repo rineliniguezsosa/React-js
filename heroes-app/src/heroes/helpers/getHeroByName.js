4efec9052f18f494132750c476f9cb6874e1eefd
rinel <rineliniguezsosa@gmail.com>
2023-03-01 12:12:17 -0600

    retornamos el filtrado donde el valor de la propiedad de superhero sea igual a el name obtenido por argumento

--- heroes-app/src/heroes/helpers/getHeroByName.js
+++ heroes-app/src/heroes/helpers/getHeroByName.js
@@ -5,4 +5,6 @@ export const getHeroByName = (name = '') =>{
     name = name.toLocaleLowerCase().trim()
 
     if(name.length===0){ return []}
+
+    return heroes.filter(heroe => heroe.superhero.toLowerCase().includes(name))
 }
