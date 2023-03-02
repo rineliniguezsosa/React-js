94fbf5fbedf044b2dc5aad302ffd30ca40b640e6
rinel <rineliniguezsosa@gmail.com>
2023-03-01 21:57:23 -0600

    importamos el componente Navigate de react-router,obtenemos el children como los elementos involucrados o hijos y vericamos si es verdadero simplemente devolvera los hijos y si no navegara hacia el login

--- heroes-app/src/router/PrivateRoute.js
+++ heroes-app/src/router/PrivateRoute.js
@@ -1,11 +1,10 @@
 import { useContext } from "react"
+import { Navigate } from "react-router-dom"
 import { AuthContext } from "../auth/context"
 
 
-export const PrivateRoute = () => {
+export const PrivateRoute = ({children}) => {
   const {logged} = useContext(AuthContext)
-  console.log(logged)
-  return (
-    <div>PrivateRoute</div>
-  )
+  //console.log(logged)
+  return (logged) ? children : <Navigate to="/login"></Navigate>
 }
