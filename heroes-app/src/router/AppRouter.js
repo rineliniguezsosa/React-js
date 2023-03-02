98f22470aef63802aca3e0274e79de8a9c10c2b9
rinel <rineliniguezsosa@gmail.com>
2023-03-01 23:25:17 -0600

    importando el componente PublicRoute,la ruta login devolvera la envoltura PublicRoute el cual devolvera la envoltura Loginpage

--- heroes-app/src/router/AppRouter.js
+++ heroes-app/src/router/AppRouter.js
@@ -2,12 +2,19 @@ import { Routes,Route } from "react-router-dom"
 import { Loginpage } from "../auth"
 import { HeroesRoutes } from "../heroes"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
+
 
 export const AppRouter = () => {
   return (
     <>
     <Routes>
-        <Route path="login" element={<Loginpage></Loginpage>}></Route>
+        <Route path="login" element={
+        <PublicRoute>
+          <Loginpage></Loginpage>
+        </PublicRoute>
+        }>
+        </Route>
     
         <Route path="/*" element={
         <PrivateRoute>
