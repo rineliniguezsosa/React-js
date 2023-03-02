188bcb3681f9dbe7374acb5a7d847fb4ebce25f1
rinel <rineliniguezsosa@gmail.com>
2023-03-02 11:50:36 -0600

    añadimos la configuracion de babal/preset-env para que se convierta el codigo de las pruebas en codigo valido javascript y definimos el tiempo de ejecucion mediante el babel/preset-react

---
+++ heroes-app/src/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+        [ '@babel/preset-env', { targets: { esmodules: true } } ], //convierte codigo de node a codigo legible
+        [ '@babel/preset-react', { runtime: 'automatic' } ] 
+        //decide el tiempo de ejecución,opcion automatic:auto importa las funciones a las que JSX transpila. classic no importa automáticamente nada.
+    ],
+};
