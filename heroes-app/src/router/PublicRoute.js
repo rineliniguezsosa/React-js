ca0017e406287ece2b3d6eb61552513e6d40ef19
rinel <rineliniguezsosa@gmail.com>
2023-03-01 23:19:01 -0600

    importando Navigate de react-router y negando el logged como sera false siempre nos navegara hacia la ruta marvel

--- heroes-app/src/router/PublicRoute.js
+++ heroes-app/src/router/PublicRoute.js
@@ -1,10 +1,10 @@
 import { useContext } from "react"
+import { Navigate } from "react-router-dom"
 import { AuthContext } from "../auth"
 
 
 export const PublicRoute = ({children}) => {
   const {logged} = useContext(AuthContext)
-  return (
-    <div>PublicRoute</div>
-  )
+  console.log(logged)
+  return (!logged) ? children : <Navigate to="/marvel"></Navigate>
 }
