f1df344a18811fc6333eff9efb6f2f8dfa3e8a38
rinel <rineliniguezsosa@gmail.com>
2023-03-02 00:06:55 -0600

    importamos useLocation de react-router,desestructuramos el pathname y el search que tendra el query parm de useLocation,concatenamos el pathname y el search en la variable ultimapagina y lo almacenamos en el localstorage

--- heroes-app/src/router/PrivateRoute.js
+++ heroes-app/src/router/PrivateRoute.js
@@ -1,10 +1,14 @@
 import { useContext } from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate,useLocation } from "react-router-dom"
 import { AuthContext } from "../auth/context"
 
 
 export const PrivateRoute = ({children}) => {
   const {logged} = useContext(AuthContext)
   
+  const {pathname,search} = useLocation()
+  const ultimapagina = pathname+search
+  localStorage.setItem('ultimapagina',ultimapagina)
+
   return (logged) ? children : <Navigate to="/login"></Navigate>
 }
