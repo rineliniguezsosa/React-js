5a7f63368c19f6b78a7a4533920a2e235b24f5bc
rinel <rineliniguezsosa@gmail.com>
2023-03-01 22:25:02 -0600

    desestructurando el children o elementos hijos invlucrados en la envotura del componente PublicRoute

--- heroes-app/src/router/PublicRoute.js
+++ heroes-app/src/router/PublicRoute.js
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { AuthContext } from "../auth"
 
 
-export const PublicRoute = () => {
+export const PublicRoute = ({children}) => {
   const {logged} = useContext(AuthContext)
   return (
     <div>PublicRoute</div>
