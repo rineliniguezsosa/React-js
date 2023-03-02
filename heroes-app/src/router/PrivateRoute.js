0d669ea85c31981ce96a03277f4d0caacd052ef7
rinel <rineliniguezsosa@gmail.com>
2023-03-01 21:50:36 -0600

    desestructuramos la propiedad logged del contexto

--- heroes-app/src/router/PrivateRoute.js
+++ heroes-app/src/router/PrivateRoute.js
@@ -3,6 +3,8 @@ import { AuthContext } from "../auth/context"
 
 
 export const PrivateRoute = () => {
+  const {logged} = useContext(AuthContext)
+  console.log(logged)
   return (
     <div>PrivateRoute</div>
   )
