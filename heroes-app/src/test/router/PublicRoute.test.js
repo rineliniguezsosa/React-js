24304b4675b37f04d0b36dc7fae6a5e74fa72c88
rinel <rineliniguezsosa@gmail.com>
2023-03-03 13:19:01 -0600

    importando screen de testing-library,renderizando el componente PublicRoute y observando en consola el contenido del componente PublicRoute

--- heroes-app/src/test/router/PublicRoute.test.js
+++ heroes-app/src/test/router/PublicRoute.test.js
@@ -1,10 +1,11 @@
-import { render } from '@testing-library/react'
+import { render,screen } from '@testing-library/react'
 import { PublicRoute } from '../../router/PublicRoute';
 
 
 describe('pruebas en PublicRoute.js', () => {
     
     test('debe de mostrar el children si no esta autenticado', () => {
-        
+        render(<PublicRoute></PublicRoute>)
+        screen.debug()
     });
 });
