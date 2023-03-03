935a9f822ed23de2b7b26a234864584a5a8f577a
rinel <rineliniguezsosa@gmail.com>
2023-03-03 14:33:07 -0600

    creando el objeto initialState  y pasandole al AuthContext el state cuando el usuario no se ha autenticado

--- heroes-app/src/test/router/PublicRoute.test.js
+++ heroes-app/src/test/router/PublicRoute.test.js
@@ -6,8 +6,11 @@ import { PublicRoute } from '../../router/PublicRoute';
 describe('pruebas en PublicRoute.js', () => {
     
     test('debe de mostrar el children si no esta autenticado', () => {
+        const initialState = {
+            logged:false
+        }
         render(
-        <AuthContext.Provider>
+        <AuthContext.Provider value={ initialState }>
             <PublicRoute></PublicRoute>
         </AuthContext.Provider>
         )
