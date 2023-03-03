e2b9ea94739c7d9b5e7930e3f3cb62e08729fc32
rinel <rineliniguezsosa@gmail.com>
2023-03-03 17:25:00 -0600

    pruebas en PrivateRoute

--- heroes-app/src/test/router/PrivateRoute.test.js
+++ heroes-app/src/test/router/PrivateRoute.test.js
@@ -1,3 +1,31 @@
+import { render,screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { AuthContext } from "../../auth";
+import { PrivateRoute } from "../../router/PrivateRoute";
+
+
+
 describe('prueba en PrivateRoute.js', () => {
-    
+
+    test('debe de mostrar el children si esta autenticado', () => {
+        const initialState = {
+            logged:true,
+            user:{id:'124',name:'rinel chan'}
+        }
+
+        render(
+        <AuthContext.Provider value={ initialState }>
+            <MemoryRouter>
+                <PrivateRoute>
+                    <h1>ruta privada</h1>
+                </PrivateRoute>
+            </MemoryRouter>
+        </AuthContext.Provider>
+        )
+        
+        expect(screen.getByText('ruta privada')).toBeTruthy();
+
+        screen.debug()
+        
+    });
 });
