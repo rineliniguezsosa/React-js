d890f42378b63316c33916cab1f0f28993709c80
rinel <rineliniguezsosa@gmail.com>
2023-03-03 13:45:12 -0600

    proveendo del contexto al PublicRoute

--- heroes-app/src/test/router/PublicRoute.test.js
+++ heroes-app/src/test/router/PublicRoute.test.js
@@ -6,7 +6,11 @@ import { PublicRoute } from '../../router/PublicRoute';
 describe('pruebas en PublicRoute.js', () => {
     
     test('debe de mostrar el children si no esta autenticado', () => {
-        render(<PublicRoute></PublicRoute>)
+        render(
+        <AuthContext.Provider>
+            <PublicRoute></PublicRoute>
+        </AuthContext.Provider>
+        )
         screen.debug()
     });
 });
