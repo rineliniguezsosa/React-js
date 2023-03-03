24f6eb7fe631a354f68442d8fae3b361543cf634
rinel <rineliniguezsosa@gmail.com>
2023-03-03 17:24:04 -0600

    agregando la opcion transformIgnorePatterns para transpilar los modulos de node

--- heroes-app/jest.config.js
+++ heroes-app/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     //especificamos el testEnviroment para el entorno de prueba
-    
+    transformIgnorePatterns: ["/node_modules/(?!query-string)/"], // transformIgnorePatterns opcion:permite la transpilacion de los modulos de node
 }
