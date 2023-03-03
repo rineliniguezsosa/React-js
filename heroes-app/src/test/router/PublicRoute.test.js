014d19425ca67ab60d549e1a8adfa00e99b1b9be
rinel <rineliniguezsosa@gmail.com>
2023-03-03 15:24:43 -0600

    importando Routes y Route de react-router

--- heroes-app/src/test/router/PublicRoute.test.js
+++ heroes-app/src/test/router/PublicRoute.test.js
@@ -1,5 +1,5 @@
 import { render,screen } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter,Routes,Route } from 'react-router-dom';
 import { AuthContext } from '../../auth';
 import { PublicRoute } from '../../router/PublicRoute';
 
