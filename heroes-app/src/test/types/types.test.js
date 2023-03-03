bc2b284e9bb32a52ec906b9c57c8f1f7dc831850
rinel <rineliniguezsosa@gmail.com>
2023-03-02 21:14:01 -0600

    esperando a que el type sea igual al objeto {login:'auth login',logout:'auth logout'}

--- heroes-app/src/test/types/types.test.js
+++ heroes-app/src/test/types/types.test.js
@@ -3,6 +3,6 @@ import { type } from "../../auth/types/types";
 
 describe('pruebas en los types.js', () => {
    test('debe ser igual al type', () => {
-    
+    expect(type).toEqual({login:'auth login',logout:'auth logout'}); 
    });
 });
