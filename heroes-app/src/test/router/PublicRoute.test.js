56e8c1739d8df6d56c77cd9b2a3decaaddcde887
rinel <rineliniguezsosa@gmail.com>
2023-03-03 15:03:16 -0600

    importando MemoryRouter

--- heroes-app/src/test/router/PublicRoute.test.js
+++ heroes-app/src/test/router/PublicRoute.test.js
@@ -1,4 +1,5 @@
 import { render,screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
 import { AuthContext } from '../../auth';
 import { PublicRoute } from '../../router/PublicRoute';
 
