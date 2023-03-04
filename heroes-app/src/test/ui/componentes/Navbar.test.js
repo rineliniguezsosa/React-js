25a4184f8afacfd26d5f3855b33a2ed3ba7231fa
rinel <rineliniguezsosa@gmail.com>
2023-03-03 21:14:57 -0600

    importando el componente Navbar,importando render y el screen de testing-library

--- heroes-app/src/test/ui/componentes/Navbar.test.js
+++ heroes-app/src/test/ui/componentes/Navbar.test.js
@@ -1,3 +1,6 @@
+import { Navbar } from "../../../ui/components/Navbar";
+import { render,screen } from "@testing-library/react";
+
 describe('pruebas en el componente Navbar.js', () => {
     test('debe de mostrar el nombre del usuario autenticado', () => {
         
