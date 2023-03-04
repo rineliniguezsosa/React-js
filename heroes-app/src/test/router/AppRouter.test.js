fa52fc8e0ffc90477d74fc0e1f8a0d037bbb5e41
rinel <rineliniguezsosa@gmail.com>
2023-03-03 19:43:58 -0600

    importando AuthContext

--- heroes-app/src/test/router/AppRouter.test.js
+++ heroes-app/src/test/router/AppRouter.test.js
@@ -1,5 +1,6 @@
 import { AppRouter } from "../../router/AppRouter";
 import {render,screen} from '@testing-library/react'
+import { AuthContext } from "../../auth";
 
 
 describe('pruebas en AppRouter.js', () => {
