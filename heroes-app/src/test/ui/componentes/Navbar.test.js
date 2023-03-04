07a6e350f7eec5709f1baffd832410bb7033db42
rinel <rineliniguezsosa@gmail.com>
2023-03-03 21:41:38 -0600

    creando el initialState donde simularemos la autenticacion del usuario y definimos la referencia de la funcion logoutlogin

--- heroes-app/src/test/ui/componentes/Navbar.test.js
+++ heroes-app/src/test/ui/componentes/Navbar.test.js
@@ -2,6 +2,11 @@ import { Navbar } from "../../../ui/components/Navbar";
 import { render,screen } from "@testing-library/react";
 
 describe('pruebas en el componente Navbar.js', () => {
+    const initialState = {
+        logged:true,
+        user:{id:'234',name:'rolando fabian'},
+        Logoutlogin:jest.fn()
+    }
     test('debe de mostrar el nombre del usuario autenticado', () => {
         
     });
