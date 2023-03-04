aa0b4568cf49f17a70e82c3a4a54c3ae5286a9c8
rinel <rineliniguezsosa@gmail.com>
2023-03-03 19:44:47 -0600

    importando MemoriRouter de react-router para la navegacion

--- heroes-app/src/test/router/AppRouter.test.js
+++ heroes-app/src/test/router/AppRouter.test.js
@@ -1,6 +1,8 @@
 import { AppRouter } from "../../router/AppRouter";
 import {render,screen} from '@testing-library/react'
 import { AuthContext } from "../../auth";
+import { MemoryRouter } from "react-router-dom";
+
 
 
 describe('pruebas en AppRouter.js', () => {
