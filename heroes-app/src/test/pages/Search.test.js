899fc32fc95ba60a738ec6d0580be66ccc17eaf4
rinel <rineliniguezsosa@gmail.com>
2023-03-05 16:38:08 -0600

    importando el componente Search.js

--- heroes-app/src/test/pages/Search.test.js
+++ heroes-app/src/test/pages/Search.test.js
@@ -1,4 +1,6 @@
 import {render,screen} from '@testing-library/react'
+import { Search } from '../../heroes/pages/Search';
+
 
 describe('prueba en Search.js', () => {
     test('debe de coincidir con el snapshot', () => {
