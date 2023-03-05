3ab657e5813673788fb62fcce175db430e74369d
rinel <rineliniguezsosa@gmail.com>
2023-03-05 17:10:48 -0600

    importando el MemoryRouter

--- heroes-app/src/test/pages/Search.test.js
+++ heroes-app/src/test/pages/Search.test.js
@@ -1,9 +1,11 @@
 import {render,screen} from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom';
 import { Search } from '../../heroes/pages/Search';
+
 
 
 describe('prueba en Search.js', () => {
     test('debe de coincidir con el snapshot', () => {
-        
+
     });
 });
