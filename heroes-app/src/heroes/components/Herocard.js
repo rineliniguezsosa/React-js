521afbdbb88f5593f6b03e6be023920224610179
rinel <rineliniguezsosa@gmail.com>
2023-03-05 18:38:09 -0600

    creando la varuiable urlImagen el cual contendra el path de las imagenes,agreamos las props src con el valor de urlImagen alt como el superhero y comentamos la linea 18

--- heroes-app/src/heroes/components/Herocard.js
+++ heroes-app/src/heroes/components/Herocard.js
@@ -1,12 +1,13 @@
 import {Link} from 'react-router-dom'
 
 export const Herocard = ({id,superhero,publisher,alter_ego,first_appearance,characters}) => {
+  const urlImagen = `../../assets/heroes/${id}.jpg`;
   return (
     <div className="col">
         <div className="card">
             <div className="row no-gutters">
                 <div className="col-4">
-                    <img src="" alt={superhero}></img>
+                    <img className="card-img" src={urlImagen} alt={superhero}></img>
                 </div>
 
                 <div className="col-8">
@@ -14,7 +15,7 @@ export const Herocard = ({id,superhero,publisher,alter_ego,first_appearance,char
                         <h5 className="card-title">{superhero}</h5>
                         <p className="card-text">{alter_ego}</p>
 
-                        <p>{characters}</p>
+                        {/* <p>{characters}</p> */}
                         <p className="card-text">
                             <small className="text-muted">{first_appearance}</small>
                         </p>
