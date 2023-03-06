f1f4dba714e0e4ab27751a8529517b7eb5bff2a3
rinel <rineliniguezsosa@gmail.com>
2023-03-05 19:52:31 -0600

    importando JournalApp y renderizando el componente JournalApp

--- journal-app/src/index.js
+++ journal-app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { JournalApp } from './JournalApp';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <JournalApp/>
   </React.StrictMode>
 );
 
