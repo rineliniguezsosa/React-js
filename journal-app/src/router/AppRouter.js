febc4ceb6d386700053b439e6ae4214dea274ef0
rinel <rineliniguezsosa@gmail.com>
2023-03-06 12:58:43 -0600

    importando las rutas Journalroutes

--- journal-app/src/router/AppRouter.js
+++ journal-app/src/router/AppRouter.js
@@ -1,5 +1,7 @@
 import { Routes,Route } from 'react-router-dom'
 import { Authroutes } from '../auth/routes/Authroutes'
+import { Journalroutes } from '../journal/routes/Journalroutes'
+
 
 export const AppRouter = () => {
   return (
