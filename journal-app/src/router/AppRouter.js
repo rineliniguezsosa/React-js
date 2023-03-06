481210f041054b9a056a7fd164aec91ef6068b74
rinel <rineliniguezsosa@gmail.com>
2023-03-06 13:04:10 -0600

    creando las ruta no coincidentes retornara el elemento Journalroutes

--- journal-app/src/router/AppRouter.js
+++ journal-app/src/router/AppRouter.js
@@ -7,6 +7,8 @@ export const AppRouter = () => {
   return (
     <Routes>
         <Route path="/auth/*" element={<Authroutes></Authroutes>}></Route>
+
+         <Route path="/*" element={<Journalroutes></Journalroutes>}></Route>
     </Routes>
   )
 }
