781a5bcf5c802e2d3b58e296ae1f1634989a59e1
rinel <rineliniguezsosa@gmail.com>
2023-03-06 15:26:08 -0600

    creando el componente con las props:item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}

--- journal-app/src/auth/pages/Loginpage.js
+++ journal-app/src/auth/pages/Loginpage.js
@@ -3,7 +3,9 @@ import { Grid } from '@mui/material'
 export const Loginpage = () => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
-        Loginpage
+        <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
+
+        </Grid>
     </Grid>
   )
 }
