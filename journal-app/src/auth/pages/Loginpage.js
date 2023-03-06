b959094c55c5eb74a65033811eff77de0b0f9dcd
rinel <rineliniguezsosa@gmail.com>
2023-03-06 15:21:34 -0600

    agregando el componente Grid con las props container, spacing={0},direction="column", alignItems="center", justifyContent="center",sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}

--- journal-app/src/auth/pages/Loginpage.js
+++ journal-app/src/auth/pages/Loginpage.js
@@ -2,6 +2,8 @@ import { Grid } from '@mui/material'
 
 export const Loginpage = () => {
   return (
-    <div>Loginpage</div>
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
+        Loginpage
+    </Grid>
   )
 }
