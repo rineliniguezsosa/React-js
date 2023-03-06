16d5abf1283ef6fa79f092d3a0684d1b340c5e63
rinel <rineliniguezsosa@gmail.com>
2023-03-06 15:29:23 -0600

    agregando el componente Typografia con las sig props variant="h5" sx={{mb:1}}

--- journal-app/src/auth/pages/Loginpage.js
+++ journal-app/src/auth/pages/Loginpage.js
@@ -4,7 +4,7 @@ export const Loginpage = () => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
         <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
-
+            <Typography variant="h5" sx={{mb:1}}>Login</Typography>
         </Grid>
     </Grid>
   )
