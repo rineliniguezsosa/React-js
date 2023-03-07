c620a723b650264d8e6cea653f93e7cc5b0e0167
rinel <rineliniguezsosa@gmail.com>
2023-03-07 15:46:02 -0600

    agregando la prop width para pantallas small el componente grid cambia a 450px

--- journal-app/src/auth/layout/AuthLayout.js
+++ journal-app/src/auth/layout/AuthLayout.js
@@ -4,7 +4,7 @@ export const AuthLayout = ({children,titulo = ''}) => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
 
-        <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
+        <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2,width:{sm:450}}}>
             <Typography variant="h5" sx={{mb:1}}>{titulo}</Typography>
         </Grid>
 
