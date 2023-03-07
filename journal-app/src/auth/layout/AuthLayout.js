341e210e6dd0d23c283559dd8aa2c7c8a941e56a
rinel <rineliniguezsosa@gmail.com>
2023-03-07 14:42:51 -0600

    importando los componentes Grid y Typography de material ui,pegando el contenido reutilizable

--- journal-app/src/auth/layout/AuthLayout.js
+++ journal-app/src/auth/layout/AuthLayout.js
@@ -1,7 +1,14 @@
-
+import { Grid,Typography } from '@mui/material'
 
 export const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
+        
+        <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
+            <Typography variant="h5" sx={{mb:1}}>Login</Typography>
+        </Grid>
+
+       
+    </Grid>
   )
 }
