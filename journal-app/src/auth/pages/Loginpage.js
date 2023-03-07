bc1edaa1cf89c2e1287a0b1248d848801f418246
rinel <rineliniguezsosa@gmail.com>
2023-03-07 14:25:17 -0600

    agregando la prop component para establecer el comportamiento del Link y agregando la diagonal a la ruta /auth/registro

--- journal-app/src/auth/pages/Loginpage.js
+++ journal-app/src/auth/pages/Loginpage.js
@@ -41,7 +41,7 @@ export const Loginpage = () => {
                 </Grid>
 
                 <Grid container direction="row" justifyContent="end">
-                    <Link to="auth/registro">Crear una cuenta</Link>
+                    <Link component={ RouterLink } to="/auth/registro">Crear una cuenta</Link>
                 </Grid>
 
             </Grid>
