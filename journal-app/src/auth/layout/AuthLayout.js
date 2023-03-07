3153e95690cbfad22d9804103357b0df1b4a7342
rinel <rineliniguezsosa@gmail.com>
2023-03-07 14:44:43 -0600

    desestructurando el children

--- journal-app/src/auth/layout/AuthLayout.js
+++ journal-app/src/auth/layout/AuthLayout.js
@@ -1,9 +1,9 @@
 import { Grid,Typography } from '@mui/material'
 
-export const AuthLayout = () => {
+export const AuthLayout = ({children}) => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
-        
+
         <Grid item xs={3} sx={{backgroundColor:'white',padding:3,borderRadius:2}}>
             <Typography variant="h5" sx={{mb:1}}>Login</Typography>
         </Grid>
