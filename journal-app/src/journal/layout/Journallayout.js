dcd80265ad6398ab15898cf295fbf17c0151f9fb
rinel <rineliniguezsosa@gmail.com>
2023-03-07 16:29:01 -0600

    comentarios de bloque

--- journal-app/src/journal/layout/Journallayout.js
+++ journal-app/src/journal/layout/Journallayout.js
@@ -3,7 +3,9 @@ import { Box } from '@mui/material'
 export const Journallayout = () => {
   return (
     <Box sx={{display:'flex'}}>
-        
+        {/* NAVBAR */}
+
+        {/* SIDEBAR */}
     </Box>
   )
 }
