55865d9e60098fbe8a431aff2b345ecb042ba243
rinel <rineliniguezsosa@gmail.com>
2023-03-08 15:50:39 -0600

    agregando el componente Typography con las props color="white" variant="h5"

--- journal-app/src/journal/views/NothingSelectedView.js
+++ journal-app/src/journal/views/NothingSelectedView.js
@@ -10,7 +10,7 @@ export const NothingSelectedView = () => {
             </Grid>
 
             <Grid item xs={12}>
-
+                <Typography color="white" variant="h5">Selecciona o crea una entrada</Typography>
             </Grid>
     </Grid>
   )
