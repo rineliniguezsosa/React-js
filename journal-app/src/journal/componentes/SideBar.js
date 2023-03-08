83fbfd872ac147f861fce10d46525dc958622fc0
rinel <rineliniguezsosa@gmail.com>
2023-03-08 12:37:16 -0600

    agregando las prop component="nav" para que el componente Box actue como el elemento nav,agregando el break point extra small con el width para pantallas small sera el valor de el drawerwidth y añadimos el flexShrink con el valor de cero para pantallas small

--- journal-app/src/journal/componentes/SideBar.js
+++ journal-app/src/journal/componentes/SideBar.js
@@ -2,7 +2,10 @@ import { Box } from '@mui/material'
 
 export const SideBar = () => {
   return (
-    <Box>
+    <Box 
+    component="nav"
+    sx={{width:{sm:{drawerWidth},flexShrink:{sm:0}}}}
+    >
 
     </Box>
   )
