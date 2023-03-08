4d9c68eb3a309e0126cd5a48957fbe7c303b625c
rinel <rineliniguezsosa@gmail.com>
2023-03-08 12:48:23 -0600

    agregando propiedades al Drawer variant,open y para pantallas extra small

--- journal-app/src/journal/componentes/SideBar.js
+++ journal-app/src/journal/componentes/SideBar.js
@@ -6,7 +6,17 @@ export const SideBar = ({drawerWidth}) => {
     component="nav"
     sx={{width:{sm:{drawerWidth},flexShrink:{sm:0}}}}
     >
+            <Drawer 
+            variant='permanent' 
+            open
+            sx={{
+                display:{xs:'block'},
+                '& .MuiDrawer-paper':{boxSizing:'border-box',width:drawerWidth}
+            }}
+            
+            >
 
+            </Drawer>
     </Box>
   )
 }
