66e8b4aebf6044aa86387c00a7e7adec68f5b0ca
rinel <rineliniguezsosa@gmail.com>
2023-03-07 22:24:39 -0600

    importando el componente MenuOutlined

--- journal-app/src/journal/componentes/Navbar.js
+++ journal-app/src/journal/componentes/Navbar.js
@@ -1,11 +1,12 @@
 import { AppBar,Toolbar,IconButton } from "@mui/material"
+import { MenuOutlined } from '@mui/icons-material'
 
 export const Navbar = () => {
   return (
     <AppBar position="fixed">
         <Toolbar>
             <IconButton>
-              
+
             </IconButton>
         </Toolbar>
     </AppBar>
