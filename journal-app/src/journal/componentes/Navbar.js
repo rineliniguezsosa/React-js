5915080ca113157d76719d9025f2ec86dc7dc52a
rinel <rineliniguezsosa@gmail.com>
2023-03-08 12:18:36 -0600

    agregando la prop margin-left para la pantalla small el valor de la prop drawerWidth

--- journal-app/src/journal/componentes/Navbar.js
+++ journal-app/src/journal/componentes/Navbar.js
@@ -4,7 +4,7 @@ import { MenuOutlined,LogoutOutlined } from '@mui/icons-material'
 export const Navbar = ({drawerWidth= 240}) => {
   return (
     <AppBar position="fixed" color="transparent" sx={{
-      width:{sm:`calc(100% -${drawerWidth}px)`}
+      width:{sm:`calc(100% -${drawerWidth}px)`},ml:{sm:`${drawerWidth}px`}
     }}>
         <Toolbar>
             <IconButton>
