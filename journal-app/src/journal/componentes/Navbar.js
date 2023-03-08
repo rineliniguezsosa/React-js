3d3983772c5501d7fec6984599643a2b96711880
rinel <rineliniguezsosa@gmail.com>
2023-03-07 22:20:52 -0600

    agregando el componente Toolbar

--- journal-app/src/journal/componentes/Navbar.js
+++ journal-app/src/journal/componentes/Navbar.js
@@ -3,7 +3,9 @@ import { AppBar,Toolbar } from "@mui/material"
 export const Navbar = () => {
   return (
     <AppBar position="fixed">
-
+        <Toolbar>
+        
+        </Toolbar>
     </AppBar>
   )
 }
