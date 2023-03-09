04552d2279910c5e49ac98b5fc934989623fef81
rinel <rineliniguezsosa@gmail.com>
2023-03-08 21:18:50 -0600

    removiendo las llaves del drawerWidth

--- journal-app/src/journal/componentes/SideBar.js
+++ journal-app/src/journal/componentes/SideBar.js
@@ -5,7 +5,7 @@ export const SideBar = ({drawerWidth}) => {
   return (
     <Box 
     component="nav"
-    sx={{width:{sm:{drawerWidth},flexShrink:{sm:0}}}}
+    sx={{width:{sm:drawerWidth,flexShrink:{sm:0}}}}
     >
             <Drawer 
             variant='permanent' 
