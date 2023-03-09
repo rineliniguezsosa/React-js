f94b5e2d87c82ac509c7526867f31a6e6783a49f
rinel <rineliniguezsosa@gmail.com>
2023-03-09 15:00:36 -0600

    moviendo la importacion del useSelector  y agregando la descripcion la cuenta esta en

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,12 +1,14 @@
-import './App.css'
 import { useSelector } from 'react-redux'
+import './App.css'
+
 
 
 function App() {
+  const { contador } = useSelector(state => state.contador)
   return (
     <div className="App">
       <header className="App-header">
-        
+        <span>la cuenta esta en: {contador}</span>
       </header>
     </div>
   );
