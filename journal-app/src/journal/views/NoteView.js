3d28e53d5185191ccf75ae839472d8249116ba28
rinel <rineliniguezsosa@gmail.com>
2023-03-08 18:45:48 -0600

    agregando la propiedad alignItems="center" el Grid padre para centrar todos los elementos hijos

--- journal-app/src/journal/views/NoteView.js
+++ journal-app/src/journal/views/NoteView.js
@@ -3,7 +3,7 @@ import { SaveOutlined } from '@mui/icons-material'
 
 export const Noteview = () => {
   return (
-    <Grid container direction="row" justifyContent="space-between" sx={{mb:1}}>
+    <Grid container direction="row" justifyContent="space-between" alignItems="center" sx={{mb:1}}>
         <Grid item>
             <Typography fontSize={39} fontWeight="light">2 de agosto,2023</Typography>
         </Grid>
