14478ae13b9be4d5cc032afafbb655a0c2e99bd5
rinel <rineliniguezsosa@gmail.com>
2023-03-08 20:19:24 -0600

    corrigiendo la importacion del icono por StartOutline

--- journal-app/src/journal/views/NothingSelectedView.js
+++ journal-app/src/journal/views/NothingSelectedView.js
@@ -1,12 +1,12 @@
 import {Grid,Typography} from '@mui/material'
-import { StartOutlined } from '@mui/icons-material'
+import { StarOutline } from '@mui/icons-material'
 
 
 export const NothingSelectedView = () => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'calc(100vh-110px)',backgroundColor:'primary',borderRadius:3}}>
             <Grid item xs={12}>
-                <StartOutlined sx={{fontSize:100,color:'black'}}></StartOutlined>
+                <StarOutline sx={{fontSize:100,color:'black'}}></StarOutline>
             </Grid>
 
             <Grid item xs={12}>
