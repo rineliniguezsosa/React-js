464e5bd418f398e5f04e175e059742abd5ce6f46
rinel <rineliniguezsosa@gmail.com>
2023-03-09 17:17:02 -0600

    agregando elementos hr y la lista desordenada

--- redux-toolkit/src/PokemonApp.js
+++ redux-toolkit/src/PokemonApp.js
@@ -3,6 +3,12 @@ export const PokemonApp = () => {
   return (
     <>
         <h1>Pokemon</h1>
+        <hr></hr>
+        <ul>
+            <li>Hola</li>
+            <li>Hola</li>
+            <li>Hola</li>
+        </ul>
     </>
   )
 }
