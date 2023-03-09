a6b2cc859e6bdf78a1f8f15c29e84e307366242c
rinel <rineliniguezsosa@gmail.com>
2023-03-08 18:15:41 -0600

    creando el segundo elemento Grid con el componente Typography con la fuente 39 y el fontWeight="light"

--- journal-app/src/journal/views/NoteView.js
+++ journal-app/src/journal/views/NoteView.js
@@ -3,7 +3,9 @@ import {Grid,Typography} from '@mui/material'
 export const Noteview = () => {
   return (
     <Grid container direction="row" justifyContent="space-between" sx={{mb:1}}>
-
+        <Grid item>
+            <Typography fontSize={39} fontWeight="light">2 de agosto,2023</Typography>
+        </Grid>
     </Grid>
   )
 }
