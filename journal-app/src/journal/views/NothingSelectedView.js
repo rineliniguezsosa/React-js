3c3cccec20c664d7fa73bf59d46bdbd6c7726dde
rinel <rineliniguezsosa@gmail.com>
2023-03-08 20:18:04 -0600

    eliminando la propiedad padding y cambiando las propiedades del color a black

--- journal-app/src/journal/views/NothingSelectedView.js
+++ journal-app/src/journal/views/NothingSelectedView.js
@@ -4,13 +4,13 @@ import { StartOutlined } from '@mui/icons-material'
 
 export const NothingSelectedView = () => {
   return (
-    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'calc(100vh-110px)',backgroundColor:'primary',padding:4,borderRadius:3}}>
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'calc(100vh-110px)',backgroundColor:'primary',borderRadius:3}}>
             <Grid item xs={12}>
-                <StartOutlined sx={{fontSize:100,color:'white'}}></StartOutlined>
+                <StartOutlined sx={{fontSize:100,color:'black'}}></StartOutlined>
             </Grid>
 
             <Grid item xs={12}>
-                <Typography color="white" variant="h5">Selecciona o crea una entrada</Typography>
+                <Typography color="black" variant="h5">Selecciona o crea una entrada</Typography>
             </Grid>
     </Grid>
   )
