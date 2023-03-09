f1ce473da7acf557b3e729f7336f7484b278afb2
rinel <rineliniguezsosa@gmail.com>
2023-03-08 19:53:16 -0600

    importando ImagenGallery de componentes

--- journal-app/src/journal/views/NoteView.js
+++ journal-app/src/journal/views/NoteView.js
@@ -1,5 +1,7 @@
 import {Grid,Typography,Button,TextField} from '@mui/material'
 import { SaveOutlined } from '@mui/icons-material'
+import { ImagenGallery } from '../componentes'
+
 
 export const Noteview = () => {
   return (
