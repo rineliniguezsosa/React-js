21df2954ba129e22a36b08288081a9cf3e5a6848
rinel <rineliniguezsosa@gmail.com>
2023-03-10 15:34:28 -0600

    importando la instancia pokemonApi y hacemos la consulta http hacia el pokeapi, en la linea 13 disparamos la accion pasandole los datos de la consulta

--- redux-toolkit/src/store/slices/pokemon/thunks.js
+++ redux-toolkit/src/store/slices/pokemon/thunks.js
@@ -1,8 +1,16 @@
+import { pokemonApi } from '../../../Api/pokemonApi'
 import { cargandopokemon,añadepokemones } from './PokemonSlice'
 
 export const getpokemon = (page = 0) =>{
     return async(dispatch,getState)=>{
         dispatch(cargandopokemon())
+
+        const req = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page +1}`)
+        const data = await req.json()
+
+        console.log(data)
+
+        dispatch(añadepokemones({pagina:page+1,pokemones:data.results}))
     }
 }
 
