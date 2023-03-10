8408e40acfc35dd3455aa66f6d1817876e789992
rinel <rineliniguezsosa@gmail.com>
2023-03-10 16:16:05 -0600

    agregando mensaje de carga del state cargando

--- redux-toolkit/src/PokemonApp.js
+++ redux-toolkit/src/PokemonApp.js
@@ -18,6 +18,7 @@ export const PokemonApp = () => {
     <>
         <h1>Pokemon</h1>
         <hr></hr>
+        { cargando ? <span>Espera un momento ....</span> : ''}
         <ul>
             <li>Hola</li>
             <li>Hola</li>
