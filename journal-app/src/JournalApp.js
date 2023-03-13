e961a8db1ada4138dfcb4304e23e9d58e0a96832
rinel <rineliniguezsosa@gmail.com>
2023-03-13 12:39:32 -0600

    importando el Provider de react-redux

--- journal-app/src/JournalApp.js
+++ journal-app/src/JournalApp.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
-
+import { Provider } from 'react-redux'
 
 export const JournalApp = () => {
   return (
