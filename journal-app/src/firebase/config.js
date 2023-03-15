4d60d2292d5ab99f47911e96882d1bb9a373e809
rinel <rineliniguezsosa@gmail.com>
2023-03-15 13:26:15 -0600

    correccion en la inicializacion de firebaseconfig

--- journal-app/src/firebase/config.js
+++ journal-app/src/firebase/config.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth' //auth
-import { getFirestore } from 'firebase/firestore/lite' //DB
+import { getAuth } from "firebase/auth" //auth
+import { getFirestore } from "firebase/firestore/lite" //DB
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
-export const FirebaseAuth = getAuth(firebaseConfig);
-export const FirebaseDB = getFirestore(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
+// const analytics = getAnalytics(FirebaseApp);
