2499b69c0a74fce5feb32720c93324037c1c491c
rinel <rineliniguezsosa@gmail.com>
2023-03-16 16:06:09 -0600

    eliminando las credenciales y el console.log,agregando en la linea 10 la opcion de inicio de sesion y agregando comentario de linea

--- journal-app/src/firebase/providers.js
+++ journal-app/src/firebase/providers.js
@@ -7,8 +7,8 @@ const provider = new GoogleAuthProvider();
 
 export const Signinwithgoogle = async() =>{
     try {
+        provider.setCustomParameters({prompt: 'select_account'}) //1)
         const result = await signInWithPopup(FirebaseAuth,provider)
-       // const credenciales = GoogleAuthProvider.credentialFromResult(result)
        const {displayName,email,photoURL,uid} = result.user
         console.log(displayName,email,photoURL,uid)
         return {
@@ -30,3 +30,13 @@ export const Signinwithgoogle = async() =>{
         
     }
 }
+
+/* comentarios de linea
+1)
+establecemos provider.setCustomParameters para pasar una solicitud de googleAuth para operaciones de inicio de sesion emergentes y redireccionamiento
+
+parametro: prompt
+valor:'select_account'
+descripcion:
+El servidor de autorización le solicita al usuario que seleccione una cuenta de usuario. Esto permite que un usuario que tiene varias cuentas en el servidor de autorización seleccione entre las múltiples cuentas para las que puede tener sesiones actuales.
+*/
