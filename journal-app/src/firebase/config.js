4bd3ae03884dda199260b0ace9fa6a76d1f0471e
rinel <rineliniguezsosa@gmail.com>
2023-03-16 14:48:57 -0600

    definiendo los datos del firebaseConfig

--- journal-app/src/firebase/config.js
+++ journal-app/src/firebase/config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore/lite" //DB
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_FIREBASE_APIKEY}`,
-  authDomain: `${process.env.REACT_APP_AUTHDOMAIN}`,
-  projectId: `${process.env.REACT_APP_PROYECTID}`,
-  storageBucket: `${process.env.REACT_APP_STORAGEBUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGINGSENDERID}`,
-  appId: `${process.env.REACT_APP_APPID}`,
-  measurementId: `${process.env.REACT_APP_MEASUREMENTID}`
+  apiKey: "AIzaSyCEUXZZlCcc-W2V0HTskkUy5H28kefletw",
+  authDomain: "react-journalapp-c914e.firebaseapp.com",
+  projectId: "react-journalapp-c914e",
+  storageBucket: "react-journalapp-c914e.appspot.com",
+  messagingSenderId: "28390085575",
+  appId: "1:28390085575:web:cb05e6c057c03efeb0ffe2",
+  measurementId: "G-E30QD6FRQD"
 };
 
 // Initialize Firebase
