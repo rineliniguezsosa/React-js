18e7f5b6c689e1edf6ba44d53a59c93f7f564b20
rinel <rineliniguezsosa@gmail.com>
2023-03-24 17:49:31 -0600

    eliminando la importacion  de useForm

--- journal-app/src/auth/pages/Registropage.js
+++ journal-app/src/auth/pages/Registropage.js
@@ -1,7 +1,6 @@
 import { AuthLayout } from "../layout/AuthLayout"
 import { Grid,Typography,TextField,Button,Link } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
-import { useForm } from "../../hooks"
 import { Formik,Form,Field } from 'formik'
 
 export const Registropage = () => {
