351267e0abb1658e6321131383510cfdf431030f
rinel <rineliniguezsosa@gmail.com>
2023-03-24 18:41:21 -0600

    agregando la prop required a los fields email y contraseña

--- journal-app/src/auth/pages/Registropage.js
+++ journal-app/src/auth/pages/Registropage.js
@@ -18,8 +18,8 @@ export const Registropage = () => {
 
     const validationSchema = Yup.object({
         displayName:Yup.string().required('El nombre es requerido'),
-        email:Yup.string().email('el correo debe de tener un @'),
-        contraseña:Yup.string().max(6,'La constraseña debe de contener más de 6 letras')
+        email:Yup.string().email('el correo debe de tener un @').required(),
+        contraseña:Yup.string().max(6,'La constraseña debe de contener más de 6 letras').required()
     })
     
   return (
