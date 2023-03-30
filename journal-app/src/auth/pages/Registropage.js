5a8c8dc65e2e7ed26d99be092148f4bcdea93cf7
rinel <rineliniguezsosa@gmail.com>
2023-03-30 15:17:36 -0600

    deshabilitando el botón crear cuenta cuando el status cambie a verificando

--- journal-app/src/auth/pages/Registropage.js
+++ journal-app/src/auth/pages/Registropage.js
@@ -77,7 +77,7 @@ export const Registropage = () => {
 
                 <Grid container spacing={2} sx={{mb:2,mt:1}}>
                     <Grid item xs={12} >
-                        <Button type="submit" variant='contained' fullWidth>Crear cuenta</Button>
+                        <Button type="submit" disabled={verificandoautenticacion} variant='contained' fullWidth>Crear cuenta</Button>
                     </Grid>
                 </Grid>
 
