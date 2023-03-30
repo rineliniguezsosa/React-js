abb6bed3f8ce15bd50690565c0ea1bd46154e2f5
rinel <rineliniguezsosa@gmail.com>
2023-03-29 18:51:14 -0600

    importando la funcion startcreatinguserwithpasswordemail

--- journal-app/src/auth/pages/Registropage.js
+++ journal-app/src/auth/pages/Registropage.js
@@ -4,11 +4,13 @@ import { useDispatch } from 'react-redux'
 import { Link as RouterLink } from 'react-router-dom'
 import { Formik,Form,Field,ErrorMessage } from 'formik'
 import * as Yup from 'yup';
+import { startcreatinguserwithpasswordemail } from "../../store/Auth"
+
 
 export const Registropage = () => {
 
     const dispatch = useDispatch()
-    
+
     const initialValues = {
         displayName:'',
         email:'',
