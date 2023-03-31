ae7bde5dd7e926898872da3b937e3d35dbd51e4e
rinel <rineliniguezsosa@gmail.com>
2023-03-30 19:23:40 -0600

    importando todo de yup

--- journal-app/src/auth/pages/Loginpage.js
+++ journal-app/src/auth/pages/Loginpage.js
@@ -3,6 +3,7 @@ import { Grid,Typography,TextField,Button,Link } from '@mui/material'
 import { Link as RouterLink } from 'react-router-dom'
 import { useForm } from '../../hooks'
 import { Formik,Form,Field } from 'formik'
+import * as Yup from 'yup';
 import { AuthLayout } from '../layout/AuthLayout'
 import { verificandoAutenticacion,startgooglesignin,startloginwithemailandpassword } from '../../store/Auth'
 import { useMemo } from 'react'
