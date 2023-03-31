194bf2341737f8403c88dbeaac504dcdefe0a982
rinel <rineliniguezsosa@gmail.com>
2023-03-31 16:25:24 -0600

    obtenemos el usuario actual o registrado mediante onAuthStateChanged

--- journal-app/src/router/AppRouter.js
+++ journal-app/src/router/AppRouter.js
@@ -10,6 +10,13 @@ import { onAuthStateChanged } from 'firebase/auth'
 export const AppRouter = () => {
   const { status } = useSelector(state => state.auth)
 
+   // eslint-disable-next-line react-hooks/rules-of-hooks
+   useEffect(()=>{
+    onAuthStateChanged(FirebaseAuth,async(user)=>{
+      console.log(user)
+    })
+  },[])
+  
   if(status === 'verificando'){
     return (
       <CheckingAuth/>
