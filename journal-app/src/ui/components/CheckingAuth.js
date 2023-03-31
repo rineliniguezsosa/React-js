1a7b4d657acf43d083ef1ee8755649ea55d41d6c
rinel <rineliniguezsosa@gmail.com>
2023-03-31 16:16:19 -0600

    cambiando la prop item por container

--- journal-app/src/ui/components/CheckingAuth.js
+++ journal-app/src/ui/components/CheckingAuth.js
@@ -4,7 +4,7 @@ export const CheckingAuth = () => {
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
 
-        <Grid item direction="row" justifyContent="center">
+        <Grid container direction="row" justifyContent="center">
             <CircularProgress color="warning"></CircularProgress>
         </Grid>
 
