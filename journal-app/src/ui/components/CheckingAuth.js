8f9f454bd36b125883bf731370be2b30ba232f74
rinel <rineliniguezsosa@gmail.com>
2023-03-31 13:59:03 -0600

    pegando el contenido del CheckingAuth

--- journal-app/src/ui/components/CheckingAuth.js
+++ journal-app/src/ui/components/CheckingAuth.js
@@ -1,7 +1,14 @@
-
+import { Grid,CircularProgress } from '@mui/material'
 
 export const CheckingAuth = () => {
   return (
-    <div>CheckingAuth</div>
+    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:'100vh',backgroundColor:'primary',padding:4}}>
+
+        <Grid item  sx={{width:{sm:450}}}>
+            <CircularProgress color="warning"></CircularProgress>
+        </Grid>
+
+        
+    </Grid>
   )
 }
