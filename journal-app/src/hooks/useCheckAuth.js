b4839901b077873a21504038bfdca51087689ec7
rinel <rineliniguezsosa@gmail.com>
2023-03-31 23:36:40 -0600

    custom hook useCheckAuth

---
+++ journal-app/src/hooks/useCheckAuth.js
@@ -0,0 +1,25 @@
+import { useSelector,useDispatch } from "react-redux"
+import { useEffect } from 'react'
+import { FirebaseAuth } from '../firebase/config'
+import { onAuthStateChanged } from 'firebase/auth'
+import { logout,login } from '../store/Auth'
+
+export const useCheckAuth = () => {
+
+    const { status } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+   // eslint-disable-next-line react-hooks/rules-of-hooks
+   useEffect(()=>{
+    onAuthStateChanged(FirebaseAuth,async(user)=>{
+      if(!user){ return dispatch(logout())}
+
+      const {uid,email,displayName,photoURL} = user
+
+      dispatch(login({uid,email,displayName,photoURL}))
+    })
+  },[])
+  
+  return {
+    status
+  }
+}
