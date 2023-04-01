7f3a6a6f8325471c9f810ae5474d9872c6521f96
rinel <rineliniguezsosa@gmail.com>
2023-04-01 00:08:10 -0600

    agregando el evento onClick el componente IconButton donde pasaremos la ejecucion de funcion Logout,creamos el arrow function Logout

--- journal-app/src/journal/componentes/Navbar.js
+++ journal-app/src/journal/componentes/Navbar.js
@@ -2,6 +2,10 @@ import { AppBar,Toolbar,IconButton,Grid,Typography } from "@mui/material"
 import { MenuOutlined,LogoutOutlined } from '@mui/icons-material'
 
 export const Navbar = ({drawerWidth= 240}) => {
+
+  const Logout = () =>{
+    
+  }
   return (
     <AppBar position="fixed" sx={{
       width:{sm:`calc(100% -${drawerWidth}px)`},ml:{sm:`${drawerWidth}px`}
@@ -14,7 +18,7 @@ export const Navbar = ({drawerWidth= 240}) => {
             <Grid container direction="row" justifyContent="space-between" alignItems="center">
                   <Typography variant="h6" noWrap component="div">JournalApp</Typography>
 
-                  <IconButton color="error">
+                  <IconButton onClick={Logout} color="error">
                     <LogoutOutlined></LogoutOutlined>
                   </IconButton>
             </Grid>
