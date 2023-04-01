61f83f7651159b8cf65c93d4cac348a2d5105096
rinel <rineliniguezsosa@gmail.com>
2023-03-31 23:43:28 -0600

    eliminando importaciones innecesarias

--- journal-app/src/router/AppRouter.js
+++ journal-app/src/router/AppRouter.js
@@ -1,12 +1,8 @@
 import { Routes,Route,Navigate } from 'react-router-dom'
 import { Authroutes } from '../auth/routes/Authroutes'
 import { Journalroutes } from '../journal/routes/Journalroutes'
-import { useSelector,useDispatch } from 'react-redux'
 import { CheckingAuth } from '../ui'
-import { useEffect } from 'react';
-import { FirebaseAuth } from '../firebase/config'
-import { onAuthStateChanged } from 'firebase/auth'
-import { logout,login } from '../store/Auth'
+
 
 
 export const AppRouter = () => {
