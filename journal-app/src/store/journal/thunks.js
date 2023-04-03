87df4b5d6a9f183c24e461abe44a07df73fcf49c
rinel <rineliniguezsosa@gmail.com>
2023-04-03 14:42:55 -0600

    tomamos la ruta hacia la coleccion de las notas para poder agregar una nota nueva

--- journal-app/src/store/journal/thunks.js
+++ journal-app/src/store/journal/thunks.js
@@ -5,5 +5,8 @@ import { FirebaseDB } from '../../firebase/config';
 export const startnewnote = () =>{
     return async(dispatch,getState)=>{
         const { uid } = getState().auth;
+
+        //tomamos el path ó ruta hacia el cloud de firestore mediante el doc
+        const newdoc = doc(collection(FirebaseDB,`/${uid}/journal/notas`))
     }
 }
