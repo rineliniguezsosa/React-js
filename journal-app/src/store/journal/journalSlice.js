f9de80c449bff8b1f35d4910df7fad751892c259
rinel <rineliniguezsosa@gmail.com>
2023-04-03 12:59:05 -0600

    agregando las propiedades messagesaved,notas y activo y comentarios en las propiedades

--- journal-app/src/store/journal/journalSlice.js
+++ journal-app/src/store/journal/journalSlice.js
@@ -4,7 +4,10 @@ import { createSlice } from '@reduxjs/toolkit'
 export const journalSlice = createSlice({
   name: 'journal',
   initialState:{
-    issaving:true
+    issaving:true, //variable para saber si almacene o no un objeto
+    messagesaved:'', //mensage de almacenamiento
+    notas:[], //aqui se almacenan los notas
+    activo:null //nota activa
   },
   reducers: {
     increment: (state) => {
