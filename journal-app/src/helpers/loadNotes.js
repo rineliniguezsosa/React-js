b9fa2ac6ba54d9993796a7308d3ece63650db1ff
rinel <rineliniguezsosa@gmail.com>
2023-04-04 17:55:29 -0600

    obtenemos la referencia de los documentos para que puedan ser leidos mediante collection

--- journal-app/src/helpers/loadNotes.js
+++ journal-app/src/helpers/loadNotes.js
@@ -6,4 +6,6 @@ export const loadnotes = async(uid = '') =>{
     if(!uid){
         throw new Error('El uid del usuario no existe')
     }
+    //referencia para obtener los documentos o registros
+    const ref = collection(FirebaseDB, `${ uid }/journal/notas`)
 }
