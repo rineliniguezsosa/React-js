c49509c4f003a37f3182d9cd994e2d0ff75eba6b
rinel <rineliniguezsosa@gmail.com>
2023-04-04 20:12:01 -0600

    importando la funcion  setactivenote

--- journal-app/src/journal/componentes/SideBarItem.js
+++ journal-app/src/journal/componentes/SideBarItem.js
@@ -1,6 +1,7 @@
 import { ListItem,ListItemButton,ListItemIcon,Grid,ListItemText } from '@mui/material'
 import { TurnedInNot } from '@mui/icons-material'
 import { useMemo } from 'react'
+import { setactivenote } from '../../store/journal'
 
 export const SideBarItem = ({title = '',body,id}) => {
   const nuevotitulo = useMemo(() => title.length >= 15 ? title.substring(0,15) + '...' : title, [title])
