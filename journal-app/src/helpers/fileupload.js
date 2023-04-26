352bcb876d2433da817fb899b534365ac4644b27
rinel <rineliniguezsosa@gmail.com>
2023-04-26 17:47:43 -0600

    validando el archivo si no existe

--- journal-app/src/helpers/fileupload.js
+++ journal-app/src/helpers/fileupload.js
@@ -1,5 +1,7 @@
 
 export const fileupload = async(file) =>{
+    if(!file){ throw new Error('No tenemos ningun archivo a subir')}
+    
     const cloudurl = "https://api.cloudinary.com/v1_1/dukze43st/upload"
 
     const formData = new FormData(); //crea el objeto form data
