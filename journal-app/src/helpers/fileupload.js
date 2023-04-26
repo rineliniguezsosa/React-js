4dc6e963682829bdc29bc880dc19eac882829c46
rinel <rineliniguezsosa@gmail.com>
2023-04-26 17:12:57 -0600

    agregamos el metodo append para agregar una clave y un valor a enviar por el formulario

--- journal-app/src/helpers/fileupload.js
+++ journal-app/src/helpers/fileupload.js
@@ -3,4 +3,5 @@ export const fileupload = async() =>{
     const cloudurl = "https://api.cloudinary.com/v1_1/dukze43st/upload"
 
     const formData = new FormData(); //crea el objeto form data
+    formData.append('upload_preset','react-journal') //calave,valor de el form
 }
