64cc023f08c5336d77fd13237d9884bcbed7a7c1
rinel <rineliniguezsosa@gmail.com>
2023-04-26 17:45:55 -0600

    agregando el input file mediante el formData.append

--- journal-app/src/helpers/fileupload.js
+++ journal-app/src/helpers/fileupload.js
@@ -1,7 +1,8 @@
 
-export const fileupload = async() =>{
+export const fileupload = async(file) =>{
     const cloudurl = "https://api.cloudinary.com/v1_1/dukze43st/upload"
 
     const formData = new FormData(); //crea el objeto form data
     formData.append('upload_preset','react-journal') //calave,valor de el form
+    formData.append('file',file)
 }
