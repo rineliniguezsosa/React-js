39b7cb993efb47ea823e98930e77ddd95c484648
rinel <rineliniguezsosa@gmail.com>
2023-04-26 17:54:11 -0600

    creando bloque try catch

--- journal-app/src/helpers/fileupload.js
+++ journal-app/src/helpers/fileupload.js
@@ -1,10 +1,16 @@
 
 export const fileupload = async(file) =>{
     if(!file){ throw new Error('No tenemos ningun archivo a subir')}
-    
+
     const cloudurl = "https://api.cloudinary.com/v1_1/dukze43st/upload"
 
     const formData = new FormData(); //crea el objeto form data
     formData.append('upload_preset','react-journal') //calave,valor de el form
     formData.append('file',file)
+
+    try {
+        
+    } catch (error) {
+        
+    }
 }
