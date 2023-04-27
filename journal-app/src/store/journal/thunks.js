93d56929ff6c19a82348975f2b88e9ad00147cf5
rinel <rineliniguezsosa@gmail.com>
2023-04-26 19:08:41 -0600

    importamos setphotostoactivenote de journalslice

--- journal-app/src/store/journal/thunks.js
+++ journal-app/src/store/journal/thunks.js
@@ -1,6 +1,6 @@
 import { addDoc,collection,doc,setDoc } from 'firebase/firestore/lite'
 import { FirebaseDB } from '../../firebase/config';
-import { addnewemptynote,setactivenote,savingnewnote,setnotes,updatenote,setsaving } from './journalSlice';
+import { addnewemptynote,setactivenote,savingnewnote,setnotes,updatenote,setsaving,setphotostoactivenote } from './journalSlice';
 import { fileupload, loadnotes } from '../../helpers';
 
 export const startnewnote = () =>{
