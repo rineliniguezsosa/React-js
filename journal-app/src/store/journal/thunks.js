a1dedd55d21e41eba667481fcd36c82971eb7c47
rinel <rineliniguezsosa@gmail.com>
2023-05-01 17:40:28 -0600

    obteniendo el uid del usuario autenticado

--- journal-app/src/store/journal/thunks.js
+++ journal-app/src/store/journal/thunks.js
@@ -67,6 +67,6 @@ export const startuploadingfiles = (files = []) =>{
 
 export const startdeletenote = () =>{
     return async(dispatch,getState)=>{
-
+        const { uid }= getState().auth;
     }
 }
