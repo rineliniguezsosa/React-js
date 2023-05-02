ae8edeb1c5a43812709d44cb677d4f268a93205b
rinel <rineliniguezsosa@gmail.com>
2023-05-02 15:27:44 -0600

    importando la funcion asincrona fileupload

--- journal-app/src/store/helpers/fileupload.test.js
+++ journal-app/src/store/helpers/fileupload.test.js
@@ -1,5 +1,8 @@
+import { fileupload } from "../../helpers/fileupload";
+
+
 describe('Pruebas en fileupload.js', () => {
-    
+
     test('debe se subir el archivo correctamente a cloudinary', () => {
         
     });
