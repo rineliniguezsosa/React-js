4c1e001a4e32b7896a544d63375be5682dbabd10
rinel <rineliniguezsosa@gmail.com>
2023-05-02 16:02:33 -0600

    convirtiendo la prueba en asincrona

--- journal-app/src/store/helpers/fileupload.test.js
+++ journal-app/src/store/helpers/fileupload.test.js
@@ -1,9 +1,9 @@
 import { fileupload } from "../../helpers/fileupload";
 
 
-describe('Pruebas en fileupload.js', () => {
+describe('Pruebas en fileupload.js',() => {
 
-    test('debe se subir el archivo correctamente a cloudinary', () => {
+    test('debe se subir el archivo correctamente a cloudinary',async() => {
         const imgurl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWP9RJSzgEcQo4C_Os5Jj6ggHijm6Z72l1BA&usqp=CAU"
     });
 });
