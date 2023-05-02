e670d67d52b63aae8e759416eb3a47fd3aae8be7
rinel <rineliniguezsosa@gmail.com>
2023-05-02 15:41:19 -0600

    creando la url de la imagen imgurl

--- journal-app/src/store/helpers/fileupload.test.js
+++ journal-app/src/store/helpers/fileupload.test.js
@@ -4,6 +4,6 @@ import { fileupload } from "../../helpers/fileupload";
 describe('Pruebas en fileupload.js', () => {
 
     test('debe se subir el archivo correctamente a cloudinary', () => {
-        
+        const imgurl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWP9RJSzgEcQo4C_Os5Jj6ggHijm6Z72l1BA&usqp=CAU"
     });
 });
