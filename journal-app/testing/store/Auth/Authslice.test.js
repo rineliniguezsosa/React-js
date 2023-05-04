2c90b76e660087352c15c7a4474aa7fd26146670
rinel <rineliniguezsosa@gmail.com>
2023-05-03 21:26:59 -0600

    creando el bloque test con el titulo: debe de regresar al estado inicial y llamarse auth

--- journal-app/testing/store/Auth/Authslice.test.js
+++ journal-app/testing/store/Auth/Authslice.test.js
@@ -2,4 +2,8 @@ import { authSlice } from "../../../src/store/Auth/Authslice";
 
 describe('pruebas en Authslice.js', () => {
     
+
+    test('debe de regresar al estado inicial y llamarse auth', () => {
+        
+    });
 });
