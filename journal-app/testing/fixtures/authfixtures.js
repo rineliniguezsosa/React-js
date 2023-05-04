f77b71e3f991c31eaba8f052057172c74bea8405
rinel <rineliniguezsosa@gmail.com>
2023-05-03 21:00:14 -0600

    creando el objeto initialState

---
+++ journal-app/testing/fixtures/authfixtures.js
@@ -0,0 +1,8 @@
+export const initialState = {
+    status:'verificando', //estado de la autenticacion
+    uid:null, //id user
+    email:null, //email user
+    displayname:null,
+    photoUrl:null,
+    mensajeserror:null
+}
