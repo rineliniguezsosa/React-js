d752ceb98f9805c47ec5475dcd735ca0c0654a8f
rinel <rineliniguezsosa@gmail.com>
2023-05-03 21:40:25 -0600

    desestructuramos el name de authSlice y esperamos a que name sea igual a 'auth' basicamente es el nombre del slice

--- journal-app/testing/store/Auth/Authslice.test.js
+++ journal-app/testing/store/Auth/Authslice.test.js
@@ -4,6 +4,7 @@ describe('pruebas en Authslice.js', () => {
     
 
     test('debe de regresar al estado inicial y llamarse auth', () => {
-        
+        const { name } = authSlice
+        expect(name).toBe('auth');
     });
 });
